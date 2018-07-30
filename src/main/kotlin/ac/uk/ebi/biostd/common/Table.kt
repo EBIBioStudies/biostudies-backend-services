a479d1b1c29690744aadc75e8b4fa315b8a2db0d
olgamelnichuk <olkin@ebi.ac.uk>
2018-07-30 14:28:43 +0100

    re-naming local variables

--- src/main/kotlin/ac/uk/ebi/biostd/common/Table.kt
+++ src/main/kotlin/ac/uk/ebi/biostd/common/Table.kt
@@ -7,25 +7,25 @@ const val NO_VALUE: String = ""
 typealias TableRow = List<String>
 
 data class Table<T : TableElement>(val idHeaderName: String) {
-    private val header: LinkedHashSet<String> = linkedSetOf(idHeaderName)
+    private val headers: LinkedHashSet<String> = linkedSetOf(idHeaderName)
     private val rows: MutableList<Map<String, String>> = mutableListOf()
 
     fun getHeaders(): List<String> {
-        return header.toList()
+        return headers.toList()
     }
 
     fun getRows(): List<TableRow> {
         return rows.map {
-            header.map { h -> it.getOrDefault(h, NO_VALUE) }
+            headers.map { header -> it.getOrDefault(header, NO_VALUE) }
         }
     }
 
     fun addRow(element: T) {
-        header.addAll(element.attributes.map { it.name })
+        headers.addAll(element.attributes.map { it.name })
 
         rows.add(
                 (mapOf(idHeaderName to element.id) +
-                        element.attributes.map { it.name to it.value }.toMap()))
+                        element.attributes.map { it.name to it.value }))
     }
 }
 
