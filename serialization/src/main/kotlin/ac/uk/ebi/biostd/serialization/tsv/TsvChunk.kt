e28ba0ede403eddd072cc8dd3994ef3b387811f4
Jhoan Manuel Muñoz Serrano <jhoanmanuelms@gmail.com>
2018-10-22 17:14:37 +0100

    - Use chunk lines instead of attributes - Add IntelliJ project files to git ignore

--- serialization/src/main/kotlin/ac/uk/ebi/biostd/serialization/tsv/TsvChunk.kt
+++ serialization/src/main/kotlin/ac/uk/ebi/biostd/serialization/tsv/TsvChunk.kt
@@ -3,13 +3,13 @@ package ac.uk.ebi.biostd.serialization.tsv
 import ac.uk.ebi.biostd.serialization.common.TSV_SEPARATOR
 import ac.uk.ebi.biostd.submission.Attribute
 
-data class PageTabChunk(
+data class TsvChunk(
     val header: List<String>,
-    val attributes: MutableList<Attribute>) {
+    val lines: MutableList<TsvChunkLine>) {
 
     constructor(body: MutableList<String>): this(body.removeAt(0).split(TSV_SEPARATOR), mutableListOf()) {
         body.forEach {
-            attributes.add(Attribute(it.substringBefore(TSV_SEPARATOR), it.substringAfter(TSV_SEPARATOR)))
+            lines.add(TsvChunkLine(it.substringBefore(TSV_SEPARATOR), it.substringAfter(TSV_SEPARATOR)))
         }
     }
 
@@ -20,7 +20,7 @@ data class PageTabChunk(
     fun <T> mapTable(initializer: (String, MutableList<Attribute>) -> T): List<T> {
         val rows: MutableList<T> = mutableListOf()
 
-        attributes.forEach {
+        lines.forEach {
             val attrs: MutableList<Attribute> = mutableListOf()
             it.value.split(TSV_SEPARATOR).forEachIndexed { index, attr -> attrs.add(Attribute(header[index + 1], attr)) }
             rows.add(initializer(it.name, attrs))
--- serialization/src/main/kotlin/ac/uk/ebi/biostd/serialization/tsv/TsvDeserializer.kt
+++ serialization/src/main/kotlin/ac/uk/ebi/biostd/serialization/tsv/TsvDeserializer.kt
@@ -20,44 +20,46 @@ import ebi.ac.uk.util.collections.ifNotEmpty
 
 class TsvDeserializer {
     fun deserialize(pageTabSubmission: String): Submission {
-        val chunks: MutableList<PageTabChunk> = chunkerize(pageTabSubmission)
-        val submissionChunk: PageTabChunk = chunks.removeFirst()
+        val chunks: MutableList<TsvChunk> = chunkerize(pageTabSubmission)
+        val submissionChunk: TsvChunk = chunks.removeFirst()
         var rootSection = Section()
         chunks.ifNotEmpty {
-            val rootSectionChunk: PageTabChunk = chunks.removeFirst()
-            rootSection = Section(type = rootSectionChunk.getType(), attributes = rootSectionChunk.attributes)
+            val rootSectionChunk: TsvChunk = chunks.removeFirst()
+            rootSection = Section(
+                    type = rootSectionChunk.getType(), attributes = createAttributes(rootSectionChunk.lines))
             processSubsections(rootSection, chunks)
         }
 
         return Submission(
                 accNo = submissionChunk.getIdentifier(),
-                title = submissionChunk.attributes.removeFirst().value,
-                attributes = submissionChunk.attributes,
+                title = submissionChunk.lines.removeFirst().value,
+                attributes = createAttributes(submissionChunk.lines),
                 section = rootSection)
     }
 
-    private fun processSubsections(section: Section, subsectionChunks: MutableList<PageTabChunk>) {
+    private fun processSubsections(section: Section, subsectionChunks: MutableList<TsvChunk>) {
         subsectionChunks.forEach {
             when (it.getType()) {
-                LinkFields.LINK.value -> section.links.addLeft(Link(it.getIdentifier(), it.attributes))
+                LinkFields.LINK.value -> section.links.addLeft(Link(it.getIdentifier(), createAttributes(it.lines)))
                 FileFields.FILE.value ->
-                    section.files.addLeft(File(name =  it.getIdentifier(), attributes = it.attributes))
+                    section.files.addLeft(File(name =  it.getIdentifier(), attributes = createAttributes(it.lines)))
                 SectionFields.LINKS.value -> section.links.addRight(LinksTable(it.mapTable(this::createLink)))
                 SectionFields.FILES.value -> section.files.addRight(FilesTable(it.mapTable(this::createFile)))
-                else -> section.subsections.addLeft(Section(type = it.getType(), attributes = it.attributes))
+                else ->
+                    section.subsections.addLeft(Section(type = it.getType(), attributes = createAttributes(it.lines)))
             }
         }
     }
 
-    private fun chunkerize(pageTabSubmission: String): MutableList<PageTabChunk> {
+    private fun chunkerize(pageTabSubmission: String): MutableList<TsvChunk> {
         var chunk: MutableList<String> = arrayListOf()
-        val chunks: MutableList<PageTabChunk> = arrayListOf()
+        val chunks: MutableList<TsvChunk> = arrayListOf()
         pageTabSubmission.split(TSV_LINE_BREAK).forEach {
             it.split(TSV_CHUNK_BREAK). forEach {
                 it.applyIfNotBlank { chunk.add(it) }
             }
 
-            chunks.add(PageTabChunk(chunk))
+            chunks.add(TsvChunk(chunk))
             chunk.clear()
         }
 
@@ -68,4 +70,7 @@ class TsvDeserializer {
 
     private fun createFile(
             file: String, attributes: MutableList<Attribute>): File = File(name = file, attributes = attributes)
+
+    private fun createAttributes(chunkLines: MutableList<TsvChunkLine>): MutableList<Attribute> =
+            chunkLines.mapTo(mutableListOf()) { Attribute(it.name, it.value) }
 }
