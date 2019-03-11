f5f2650a9cb30eee0399769c91644ae710ab4fc0
Juan Camilo Rada Mesa <jcamilorada@ebi.ac.uk>
2019-03-11 09:49:10 +0000

    code review changes

--- scheduler/scheduler/src/main/kotlin/ac/uk/ebi/pmc/scheduler/pmc/importer/PmcImporterResource.kt
+++ scheduler/scheduler/src/main/kotlin/ac/uk/ebi/pmc/scheduler/pmc/importer/PmcImporterResource.kt
@@ -1,6 +1,5 @@
 package ac.uk.ebi.pmc.scheduler.pmc.importer
 
-import ac.uk.ebi.cluster.client.model.Job
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RestController
@@ -10,12 +9,8 @@ import java.io.File
 class PmcImporterResource(private val importerService: PmcLoaderService) {
 
     @PostMapping("/api/pmc/load/folder")
-    fun submitFile(@RequestHeader("path") path: String): Job {
-        return importerService.loadFile(File(path))
-    }
+    fun submitFile(@RequestHeader("path") path: String) = importerService.loadFile(File(path))
 
     @PostMapping("/api/pmc/process")
-    fun process(): Job {
-        return importerService.process()
-    }
+    fun process() = importerService.process()
 }
