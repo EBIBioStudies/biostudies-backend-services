3c34474fa318d75121fa9350728ffe6dc48b5242
Jhoan Manuel Muñoz Serrano <jhoanmanuelms@gmail.com>
2019-03-12 13:56:30 +0000

    Fix File Reading (#39)
    Fix file text reading to be only on demand

--- submission/submitter/src/main/kotlin/ac/uk/ebi/biostd/submission/model/ResourceFile.kt
+++ submission/submitter/src/main/kotlin/ac/uk/ebi/biostd/submission/model/ResourceFile.kt
@@ -4,10 +4,6 @@ import ebi.ac.uk.io.asString
 import java.io.InputStream
 
 data class ResourceFile(val name: String, val inputStream: InputStream, val size: Long) {
-    var text: String = ""
-
-    // TODO Improve the file reading. This should performed only when necessary
-    init {
-        text = inputStream.asString()
-    }
+    val text: String
+        get() = inputStream.asString()
 }
