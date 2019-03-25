4c5e7dc702a714794e37d53f86532f96e99f709c
Jhoan Manuel Muñoz Serrano <jhoanmanuelms@gmail.com>
2019-03-25 10:03:01 +0000

    JSON Files Serialization
    Remove unnecessary line

--- commons/commons-serialization/src/main/kotlin/ac/uk/ebi/biostd/json/deserialization/FileJsonDeserializer.kt
+++ commons/commons-serialization/src/main/kotlin/ac/uk/ebi/biostd/json/deserialization/FileJsonDeserializer.kt
@@ -9,18 +9,16 @@ import com.fasterxml.jackson.databind.JsonNode
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.deser.std.StdDeserializer
 import com.fasterxml.jackson.databind.node.TextNode
-import ebi.ac.uk.model.Attribute
 import ebi.ac.uk.model.File
 import ebi.ac.uk.model.constants.FileFields
 
 class FileJsonDeserializer : StdDeserializer<File>(File::class.java) {
-
     override fun deserialize(jp: JsonParser, ctxt: DeserializationContext): File {
         val mapper = jp.codec as ObjectMapper
         val node: JsonNode = mapper.readTree(jp)
-        val attrs: MutableList<Attribute> = mapper.convertList(node.findNode<JsonNode>(FileFields.ATTRIBUTES.value))
 
         return File(
-            path = node.getNode<TextNode>(FileFields.PATH.value).textValue(), attributes = attrs)
+            path = node.getNode<TextNode>(FileFields.PATH.value).textValue(),
+            attributes = mapper.convertList(node.findNode<JsonNode>(FileFields.ATTRIBUTES.value)))
     }
 }
