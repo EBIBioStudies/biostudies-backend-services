fbc6fb49f5be0da902b8695292012657a1bf3db6
Jhoan Manuel Muñoz Serrano <jhoanmanuelms@gmail.com>
2019-06-13 15:25:02 +0100

    Fix Access Tag
    - Private submissions shouldn't inherit the "Public" access tag if its parent if public
    - Fix library file serialization
    - Fix typos in configuration files

--- submission/submitter/src/main/kotlin/ac/uk/ebi/biostd/submission/processors/AccessTagProcessor.kt
+++ submission/submitter/src/main/kotlin/ac/uk/ebi/biostd/submission/processors/AccessTagProcessor.kt
@@ -1,14 +1,15 @@
 package ac.uk.ebi.biostd.submission.processors
 
 import ebi.ac.uk.model.ExtendedSubmission
+import ebi.ac.uk.model.extensions.releaseDate
 import ebi.ac.uk.persistence.PersistenceContext
 
 /**
  * Add all parent submission tags to submitted submission.
  */
 class AccessTagProcessor : SubmissionProcessor {
-
     override fun process(submission: ExtendedSubmission, context: PersistenceContext) {
         submission.accessTags.addAll(context.getParentAccessTags(submission))
+        submission.accessTags.removeIf { it == "Public" && submission.releaseDate.isNullOrBlank().not() }
     }
 }
---
+++ submission/submitter/src/test/kotlin/ac/uk/ebi/biostd/submission/processors/AccessTagProcessorTest.kt
@@ -0,0 +1,39 @@
+package ac.uk.ebi.biostd.submission.processors
+
+import ac.uk.ebi.biostd.submission.test.createBasicExtendedSubmission
+import ebi.ac.uk.model.extensions.releaseDate
+import ebi.ac.uk.persistence.PersistenceContext
+import io.mockk.every
+import io.mockk.impl.annotations.MockK
+import io.mockk.junit5.MockKExtension
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+
+@ExtendWith(MockKExtension::class)
+class AccessTagProcessorTest(@MockK private val mockPersistenceContext: PersistenceContext) {
+    private val testInstance = AccessTagProcessor()
+    private val testSubmission = createBasicExtendedSubmission()
+
+    @BeforeEach
+    fun setUp() {
+        every { mockPersistenceContext.getParentAccessTags(testSubmission) } returns listOf("Public")
+    }
+
+    @Test
+    fun process() {
+        testInstance.process(testSubmission, mockPersistenceContext)
+
+        assertThat(testSubmission.accessTags).hasSize(1)
+        assertThat(testSubmission.accessTags.first()).isEqualTo("Public")
+    }
+
+    @Test
+    fun `private submission with public parent`() {
+        testSubmission.releaseDate = "2030-12-31"
+        testInstance.process(testSubmission, mockPersistenceContext)
+
+        assertThat(testSubmission.accessTags).isEmpty()
+    }
+}
