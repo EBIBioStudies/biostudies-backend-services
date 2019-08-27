73608cf680f7db6ce0b1c11a1f964ebd8106fe19
Awais Athar <awais@ebi.ac.uk>
2019-08-27 14:22:58 +0100

    Allow gradle script to set up test db on windows

---
+++ infrastructure/src/main/resources/setup/database/SetUpDatabase.cmd
@@ -0,0 +1,8 @@
+docker build -t biostudies-mysql:0.2 -f Dockerfile.%1 .
+docker run -d ^
+  --name biostudies-mysql ^
+  -e MYSQL_ROOT_PASSWORD=admin ^
+  -e MYSQL_DATABASE=BioStudiesDev ^
+  -p 3306:3306 ^
+  biostudies-mysql:0.2 ^
+  --lower-case-table-names=1
