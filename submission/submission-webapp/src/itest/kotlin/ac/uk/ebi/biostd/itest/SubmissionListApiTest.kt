ae237cdd27e2f480508f3ccc6f9e19e6b0ece4f7
Jhoan Manuel Muñoz Serrano <jhoanmanuelms@gmail.com>
2019-10-07 16:33:37 +0100

    Pivotal ID # 168727325: Submission API (#125)
    * Model In Repository
    - Return model instead of persistence object in submissions repository
    - Change filters to OffsetDate
    - Remove unnecessary headers
    - Add version in response
    - Update the tests and add an integration test to the filtering by release date

--- submission/submission-webapp/src/itest/kotlin/ac/uk/ebi/biostd/itest/SubmissionListApiTest.kt
+++ submission/submission-webapp/src/itest/kotlin/ac/uk/ebi/biostd/itest/SubmissionListApiTest.kt
@@ -9,8 +9,10 @@ import ac.uk.ebi.biostd.itest.common.BaseIntegrationTest
 import ac.uk.ebi.biostd.itest.common.TestConfig
 import ac.uk.ebi.biostd.itest.entities.RegularUser
 import ac.uk.ebi.biostd.itest.entities.SuperUser
+import ac.uk.ebi.biostd.persistence.model.AccessTag
+import ac.uk.ebi.biostd.persistence.repositories.TagsDataRepository
 import ebi.ac.uk.dsl.submission
-import ebi.ac.uk.model.extensions.releaseTime
+import ebi.ac.uk.model.extensions.releaseDate
 import ebi.ac.uk.model.extensions.title
 import io.github.glytching.junit.extension.folder.TemporaryFolder
 import io.github.glytching.junit.extension.folder.TemporaryFolderExtension
@@ -19,12 +21,12 @@ import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.web.server.LocalServerPort
 import org.springframework.context.annotation.Import
 import org.springframework.test.annotation.DirtiesContext
 import org.springframework.test.context.junit.jupiter.SpringExtension
-import java.time.OffsetDateTime
 
 @ExtendWith(TemporaryFolderExtension::class)
 internal class SubmissionListApiTest(tempFolder: TemporaryFolder) : BaseIntegrationTest(tempFolder) {
@@ -33,7 +35,7 @@ internal class SubmissionListApiTest(tempFolder: TemporaryFolder) : BaseIntegrat
     @Import(value = [TestConfig::class, SubmitterConfig::class, PersistenceConfig::class, TestConfig::class])
     @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
     @DirtiesContext
-    inner class SingleSubmissionTest {
+    inner class SingleSubmissionTest(@Autowired val tagsDataRepository: TagsDataRepository) {
         @LocalServerPort
         private var serverPort: Int = 0
 
@@ -44,13 +46,15 @@ internal class SubmissionListApiTest(tempFolder: TemporaryFolder) : BaseIntegrat
             val securityClient = SecurityWebClient.create("http://localhost:$serverPort")
             securityClient.registerUser(SuperUser.asRegisterRequest())
             securityClient.registerUser(RegularUser.asRegisterRequest())
+            tagsDataRepository.save(AccessTag(name = "Public"))
 
             webClient = securityClient.getAuthenticatedClient(SuperUser.email, SuperUser.password)
-            for (i in 1..20) {
-                val submission = submission("SimpleAcc$i") {
-                    title = "Simple Submission $i - keyword$i"
-                    releaseTime = OffsetDateTime.now().plusDays(i.toLong())
+            for (idx in 11..30) {
+                val submission = submission("SimpleAcc$idx") {
+                    title = "Simple Submission $idx - keyword$idx"
+                    releaseDate = "2019-09-$idx"
                 }
+
                 webClient.submitSingle(submission, SubmissionFormat.JSON)
             }
         }
@@ -58,6 +62,7 @@ internal class SubmissionListApiTest(tempFolder: TemporaryFolder) : BaseIntegrat
         @Test
         fun `get submission list`() {
             val submissionList = webClient.getSubmissions()
+
             assertThat(submissionList).isNotNull
             assertThat(submissionList).hasSize(15)
             assertThat(submissionList).isSortedAccordingTo { a, b -> b.rtime.compareTo(a.rtime) }
@@ -66,10 +71,12 @@ internal class SubmissionListApiTest(tempFolder: TemporaryFolder) : BaseIntegrat
         @Test
         fun `get submission list by accession`() {
             val submissionList = webClient.getSubmissions(mapOf(
-                "accNo" to "SimpleAcc10"
+                "accNo" to "SimpleAcc17"
             ))
+
             assertThat(submissionList).hasOnlyOneElementSatisfying {
-                assertThat(it.accno).isEqualTo("SimpleAcc10")
+                assertThat(it.accno).isEqualTo("SimpleAcc17")
+                assertThat(it.version).isEqualTo(1)
             }
         }
 
@@ -78,9 +85,20 @@ internal class SubmissionListApiTest(tempFolder: TemporaryFolder) : BaseIntegrat
             val submissionList = webClient.getSubmissions(mapOf(
                 "keywords" to "keyword20"
             ))
+
             assertThat(submissionList).hasOnlyOneElementSatisfying {
                 assertThat(it.title).contains("keyword20")
             }
+        }
+
+        @Test
+        fun `get submission list by release date`() {
+            val submissionList = webClient.getSubmissions(mapOf(
+                "rTimeFrom" to "2019-09-24T09:41:44.000Z",
+                "rTimeTo" to "2019-09-28T09:41:44.000Z"
+            ))
+
+            assertThat(submissionList).hasSize(4)
         }
 
         @Test
@@ -88,6 +106,7 @@ internal class SubmissionListApiTest(tempFolder: TemporaryFolder) : BaseIntegrat
             val submissionList = webClient.getSubmissions(mapOf(
                 "offset" to 15
             ))
+
             assertThat(submissionList).hasSize(5)
         }
     }
