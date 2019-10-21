8f509588bbbbef71697b046fd31f1cf71c8e6d4e
Jhoan Munoz <jhoanmanuelms@gmail.com>
2019-10-21 14:22:44 +0100

    Excel Stream Reader
    Code review comments

--- commons/commons-util/src/main/kotlin/ebi/ac/uk/util/file/ExcelReader.kt
+++ commons/commons-util/src/main/kotlin/ebi/ac/uk/util/file/ExcelReader.kt
@@ -3,25 +3,22 @@ package ebi.ac.uk.util.file
 import com.monitorjbl.xlsx.StreamingReader
 import ebi.ac.uk.dsl.Tsv
 import ebi.ac.uk.dsl.TsvLine
+import org.apache.poi.ss.usermodel.Sheet
 import java.io.File
 
 const val BUFFER_SIZE = 4096
 const val ROW_CACHE_SIZE = 1000
 
 class ExcelReader {
-    fun readContentAsTsv(file: File): String {
-        val sheet = file.inputStream().use {
-            StreamingReader.builder()
-                .rowCacheSize(ROW_CACHE_SIZE)
-                .bufferSize(BUFFER_SIZE)
-                .open(it)
-                .getSheetAt(0)
-        }
+    fun readContentAsTsv(file: File): String =
+        StreamingReader.builder()
+            .rowCacheSize(ROW_CACHE_SIZE)
+            .bufferSize(BUFFER_SIZE)
+            .open(file)
+            .use { Tsv(getTsvLines(it.getSheetAt(0))).toString() }
 
-        val tsvLines = sheet.map(
+    private fun getTsvLines(sheet: Sheet) =
+        sheet.map(
             { TsvLine(it.map { cell -> cell.valueAsString }) },
             { TsvLine() })
-
-        return Tsv(tsvLines).toString()
-    }
 }
