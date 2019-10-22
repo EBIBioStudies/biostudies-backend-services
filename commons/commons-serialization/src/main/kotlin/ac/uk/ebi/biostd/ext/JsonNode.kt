4d46efcaf7351039ae0a3bf2bd25350591e9e2cb
Juan Camilo Rada Mesa <jcamilorada@ebi.ac.uk>
2019-10-22 16:28:21 +0100

    code review changes

--- commons/commons-serialization/src/main/kotlin/ac/uk/ebi/biostd/ext/JsonNode.kt
+++ commons/commons-serialization/src/main/kotlin/ac/uk/ebi/biostd/ext/JsonNode.kt
@@ -3,20 +3,20 @@ package ac.uk.ebi.biostd.ext
 import com.fasterxml.jackson.databind.JsonNode
 import com.fasterxml.jackson.databind.node.NullNode
 
-internal inline fun <reified T : JsonNode> JsonNode.findNode(property: String): T? {
-    return when (val node = get(property)) {
+internal inline fun <reified T : JsonNode> JsonNode.findNode(property: String): T? =
+    when (val node = get(property)) {
         is NullNode -> null
         null -> null
         else -> ensureType<T>(node, property)
     }
-}
 
-internal inline fun <reified T : JsonNode> JsonNode.getNode(property: String): T {
-    return when (val node = get(property)) {
+
+internal inline fun <reified T : JsonNode> JsonNode.getNode(property: String): T =
+    when (val node = get(property)) {
         is NullNode, null -> throw IllegalStateException("Expecting to find property with '$property' in node '$this'")
         else -> ensureType(node, property)
     }
-}
+
 
 private inline fun <reified T : JsonNode?> JsonNode.ensureType(node: JsonNode, property: String): T {
     require(node is T) {
