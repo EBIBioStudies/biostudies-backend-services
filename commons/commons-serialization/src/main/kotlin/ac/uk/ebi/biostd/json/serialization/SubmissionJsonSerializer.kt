53ba17f4511f6bd5ae0738e089ab8a67ee370138
Juan Camilo Rada Mesa <jcamilorada@ebi.ac.uk>
2019-10-30 09:17:44 +0000

    pivotal-169315694: Release date retrieve as ISO (bug) - added iso date handling

--- commons/commons-serialization/src/main/kotlin/ac/uk/ebi/biostd/json/serialization/SubmissionJsonSerializer.kt
+++ commons/commons-serialization/src/main/kotlin/ac/uk/ebi/biostd/json/serialization/SubmissionJsonSerializer.kt
@@ -1,7 +1,6 @@
 package ac.uk.ebi.biostd.json.serialization
 
 import ac.uk.ebi.biostd.json.common.writeJsonArray
-import ac.uk.ebi.biostd.json.common.writeJsonNumber
 import ac.uk.ebi.biostd.json.common.writeJsonObject
 import ac.uk.ebi.biostd.json.common.writeJsonString
 import ac.uk.ebi.biostd.json.common.writeObj
@@ -11,7 +10,7 @@ import com.fasterxml.jackson.databind.ser.std.StdSerializer
 import ebi.ac.uk.model.ExtendedSubmission
 import ebi.ac.uk.model.Submission
 import ebi.ac.uk.model.constants.SubFields
-import ebi.ac.uk.util.collections.merge
+import java.time.format.DateTimeFormatter
 
 internal class SubmissionJsonSerializer : StdSerializer<Submission>(Submission::class.java) {
     override fun serialize(subm: Submission, gen: JsonGenerator, provider: SerializerProvider) {
@@ -21,11 +20,7 @@ internal class SubmissionJsonSerializer : StdSerializer<Submission>(Submission::
             writeJsonObject(SubFields.SECTION, subm.section)
 
             if (subm is ExtendedSubmission) {
-                writeJsonArray(SubFields.ACCESS_TAGS, merge(subm.accessTags), gen::writeString)
-                writeJsonNumber(SubFields.RELEASE_TIME, subm.releaseTime.toEpochSecond())
-                writeJsonNumber(SubFields.CREATION_TIME, subm.creationTime.toEpochSecond())
-                writeJsonNumber(SubFields.MODIFICATION_TIME, subm.modificationTime.toEpochSecond())
-                writeJsonString(SubFields.SECRET, subm.secretKey)
+                writeJsonString(SubFields.RELEASE_TIME, subm.releaseTime.format(DateTimeFormatter.ISO_DATE))
             }
         }
     }
---
+++ commons/commons-serialization/src/test/kotlin/ac/uk/ebi/biostd/json/serialization/SubmissionJsonSerializerTest.kt
@@ -0,0 +1,70 @@
+package ac.uk.ebi.biostd.json.serialization
+
+import com.fasterxml.jackson.annotation.JsonInclude.Include
+import com.fasterxml.jackson.databind.DeserializationFeature
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.module.SimpleModule
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import ebi.ac.uk.dsl.attribute
+import ebi.ac.uk.dsl.json.jsonArray
+import ebi.ac.uk.dsl.json.jsonObj
+import ebi.ac.uk.dsl.submission
+import ebi.ac.uk.model.ExtendedSubmission
+import ebi.ac.uk.model.Submission
+import ebi.ac.uk.model.User
+import io.mockk.impl.annotations.MockK
+import io.mockk.junit5.MockKExtension
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.skyscreamer.jsonassert.JSONAssert.assertEquals
+import org.skyscreamer.jsonassert.JSONCompareMode.LENIENT
+import java.time.OffsetDateTime
+import java.time.ZoneOffset
+
+@ExtendWith(MockKExtension::class)
+internal class SubmissionJsonSerializerTest {
+
+    private val testInstance = createSerializer()
+
+    private val submission = submission("abc123") { attribute("attr-name", "attr-value") }
+
+    @Test
+    fun `serialize submission`() {
+        val json = testInstance.writeValueAsString(submission)
+        val expected = jsonObj {
+            "accno" to "abc123"
+            "section" to jsonObj { }
+            "attributes" to jsonArray(jsonObj { "reference" to false; "name" to "attr-name"; "value" to "attr-value" })
+        }
+        assertEquals("invalid submission jso", json, expected.toString(), LENIENT)
+    }
+
+    @Test
+    fun `serialize extended submission`(@MockK user: User) {
+        val dateTime = OffsetDateTime.of(2018, 12, 31, 0, 0, 0, 0, ZoneOffset.UTC)
+        val extendedSubmission = ExtendedSubmission(submission, user).apply { releaseTime = dateTime }
+
+        val json = testInstance.writeValueAsString(extendedSubmission)
+        val expected = jsonObj {
+            "accno" to "abc123"
+            "section" to jsonObj { }
+            "rtime" to "2018-12-31Z"
+            "attributes" to jsonArray(jsonObj { "reference" to false; "name" to "attr-name"; "value" to "attr-value" })
+        }
+        assertEquals("invalid submission jso", json, expected.toString(), LENIENT)
+    }
+
+    companion object {
+
+        fun createSerializer(): ObjectMapper {
+            val module = SimpleModule()
+            module.addSerializer(Submission::class.java, SubmissionJsonSerializer())
+
+            return jacksonObjectMapper().apply {
+                registerModule(module)
+                setSerializationInclusion(Include.NON_EMPTY)
+                configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+            }
+        }
+    }
+}
