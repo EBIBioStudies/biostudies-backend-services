4fc793ea6b1953e1a13bb226576d1f56ff02eb6c
Jhoan Manuel Muñoz Serrano <jhoanmanuelms@gmail.com>
2020-03-17 16:18:16 +0000

    Pivotal ID # 138327043: Email User Successful Submission
    Fix build

--- submission/submitter/src/main/kotlin/ac/uk/ebi/biostd/submission/events/SubmissionEvents.kt
+++ submission/submitter/src/main/kotlin/ac/uk/ebi/biostd/submission/events/SubmissionEvents.kt
@@ -3,7 +3,7 @@ package ac.uk.ebi.biostd.submission.events
 import ebi.ac.uk.model.User
 import io.reactivex.subjects.PublishSubject
 
-class SuccessfulSubmission(val user: User, val accNo: String, val title: String?)
+class SuccessfulSubmission(val user: User, val accNo: String, val title: String)
 
 object SubmissionEvents {
     val successfulSubmission by lazy { PublishSubject.create<SuccessfulSubmission>() }
