4a55276c929d146716dfb7a8109c692fd2993446
Jhoan Manuel Muñoz Serrano <jhoanmanuelms@gmail.com>
2020-04-06 20:01:09 +0100

    Pivotal ID # 138327043: Email User Successful Submission (#192)
    Fix database table name

--- submission/notifications/src/main/kotlin/ebi/ac/uk/notifications/persistence/service/NotificationPersistenceService.kt
+++ submission/notifications/src/main/kotlin/ebi/ac/uk/notifications/persistence/service/NotificationPersistenceService.kt
@@ -1,8 +1,8 @@
 package ebi.ac.uk.notifications.persistence.service
 
-import ebi.ac.uk.notifications.persistence.model.SubmissionRt
+import ebi.ac.uk.notifications.persistence.model.SubmissionRT
 import ebi.ac.uk.notifications.persistence.repositories.SubmissionRtRepository
 
 class NotificationPersistenceService(private val submissionRtRepository: SubmissionRtRepository) {
-    fun saveRtNotification(accNo: String, ticketId: String) = submissionRtRepository.save(SubmissionRt(accNo, ticketId))
+    fun saveRtNotification(accNo: String, ticketId: String) = submissionRtRepository.save(SubmissionRT(accNo, ticketId))
 }
