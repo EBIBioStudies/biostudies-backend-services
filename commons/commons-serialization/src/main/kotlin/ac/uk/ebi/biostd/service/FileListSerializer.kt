a4838b927c08103b14caa5b47086d1a8e47d4632
Jhoan Manuel Muñoz Serrano <jhoanmanuelms@gmail.com>
2020-04-06 17:34:46 +0100

    Pivotal ID # 172080341: File List Independant Format (#190)
    * Pivotal ID # 172080341: File List Independant Format
    
    - Deserialize the file list according to its extension
    - The file list format is independent from the submission format
    - Add unit and integration tests

--- commons/commons-serialization/src/main/kotlin/ac/uk/ebi/biostd/service/FileListSerializer.kt
+++ commons/commons-serialization/src/main/kotlin/ac/uk/ebi/biostd/service/FileListSerializer.kt
@@ -1,25 +1,42 @@
 package ac.uk.ebi.biostd.service
 
 import ac.uk.ebi.biostd.integration.SubFormat
+import ac.uk.ebi.biostd.integration.SubFormat.Companion.JSON
+import ac.uk.ebi.biostd.integration.SubFormat.Companion.TSV
+import ac.uk.ebi.biostd.integration.SubFormat.Companion.XML
+import ac.uk.ebi.biostd.integration.SubFormat.JsonFormat
+import ac.uk.ebi.biostd.integration.SubFormat.TsvFormat
+import ac.uk.ebi.biostd.integration.SubFormat.XmlFormat
 import ebi.ac.uk.io.sources.FilesSource
 import ebi.ac.uk.model.FileList
 import ebi.ac.uk.model.FilesTable
 import ebi.ac.uk.model.Submission
 import ebi.ac.uk.model.extensions.allSections
 import ebi.ac.uk.model.extensions.fileListName
+import ebi.ac.uk.util.file.ExcelReader
+import java.io.File
 
-internal class FileListSerializer(private val serializer: PagetabSerializer) {
-    internal fun deserializeFileList(submission: Submission, format: SubFormat, source: FilesSource): Submission {
+internal class FileListSerializer(
+    private val excelReader: ExcelReader,
+    private val serializer: PagetabSerializer
+) {
+    internal fun deserializeFileList(submission: Submission, source: FilesSource): Submission {
         submission.allSections()
             .filter { section -> section.fileListName != null }
             .map { section -> section to section.fileListName!! }
-            .forEach { (section, fileList) -> section.fileList = getFileList(fileList, format, source) }
+            .forEach { (section, fileList) -> section.fileList = getFileList(fileList, source) }
         return submission
     }
 
-    private fun getFileList(fileList: String, format: SubFormat, source: FilesSource): FileList {
-        val fileContent = source.getFile(fileList).readText()
-        val filesTable = serializer.deserializeElement<FilesTable>(fileContent, format)
+    private fun getFileList(fileList: String, source: FilesSource): FileList {
+        val filesTable = getFilesTable(source.getFile(fileList))
         return FileList(fileList, filesTable.elements)
+    }
+
+    private fun getFilesTable(file: File): FilesTable = when (SubFormat.fromExtension(file.extension)) {
+        XmlFormat -> serializer.deserializeElement(file.readText(), XML)
+        is JsonFormat -> serializer.deserializeElement(file.readText(), JSON)
+        TsvFormat.Tsv -> serializer.deserializeElement(file.readText(), TSV)
+        TsvFormat.XlsxTsv -> serializer.deserializeElement(excelReader.readContentAsTsv(file), TSV)
     }
 }
---
+++ submission/submission-webapp/src/itest/kotlin/ac/uk/ebi/biostd/itest/test/submission/submit/FileListSubmissionTest.kt
@@ -0,0 +1,145 @@
+package ac.uk.ebi.biostd.itest.test.submission.submit
+
+import ac.uk.ebi.biostd.client.integration.commons.SubmissionFormat
+import ac.uk.ebi.biostd.client.integration.web.BioWebClient
+import ac.uk.ebi.biostd.common.config.PersistenceConfig
+import ac.uk.ebi.biostd.itest.common.BaseIntegrationTest
+import ac.uk.ebi.biostd.itest.entities.SuperUser
+import ac.uk.ebi.biostd.persistence.service.SubmissionRepository
+import ebi.ac.uk.asserts.assertThat
+import ebi.ac.uk.dsl.excel.excel
+import ebi.ac.uk.dsl.json.jsonArray
+import ebi.ac.uk.dsl.json.jsonObj
+import ebi.ac.uk.dsl.line
+import ebi.ac.uk.dsl.tsv
+import ebi.ac.uk.model.Attribute
+import ebi.ac.uk.model.File
+import ebi.ac.uk.model.FileList
+import ebi.ac.uk.model.extensions.fileListName
+import ebi.ac.uk.test.createFile
+import io.github.glytching.junit.extension.folder.TemporaryFolder
+import io.github.glytching.junit.extension.folder.TemporaryFolderExtension
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.web.server.LocalServerPort
+import org.springframework.context.annotation.Import
+import org.springframework.test.annotation.DirtiesContext
+import org.springframework.test.context.junit.jupiter.SpringExtension
+import org.springframework.transaction.annotation.Transactional
+import java.nio.file.Paths
+
+@ExtendWith(TemporaryFolderExtension::class)
+internal class FileListSubmissionTest(private val tempFolder: TemporaryFolder) : BaseIntegrationTest(tempFolder) {
+    @Nested
+    @Import(PersistenceConfig::class)
+    @ExtendWith(SpringExtension::class)
+    @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+    @Transactional
+    @DirtiesContext
+    inner class MixedFormatFileListSubmissionTest(@Autowired private val submissionRepository: SubmissionRepository) {
+        @LocalServerPort
+        private var serverPort: Int = 0
+
+        private lateinit var webClient: BioWebClient
+
+        @BeforeAll
+        fun init() {
+            webClient = getWebClient(serverPort, SuperUser)
+        }
+
+        @Test
+        fun `JSON submission with TSV file list`() {
+            val submission = jsonObj {
+                "accno" to "S-TEST4"
+                "attributes" to jsonArray({
+                    "name" to "Title"
+                    "value" to "Test Submission"
+                })
+                "section" to {
+                    "accno" to "SECT-001"
+                    "type" to "Study"
+                    "attributes" to jsonArray({
+                        "name" to "Title"
+                        "value" to "Root Section"
+                    }, {
+                        "name" to "File List"
+                        "value" to "FileList.tsv"
+                    })
+                }
+            }.toString()
+
+            val fileList = tempFolder.createFile(
+                "FileList.tsv",
+                tsv {
+                    line("Files", "GEN")
+                    line("File4.txt", "ABC")
+                }.toString())
+
+            val response = webClient.submitSingle(submission, SubmissionFormat.JSON, listOf(fileList, tempFolder.createFile("File4.txt")))
+            assertThat(response).isSuccessful()
+            assertSubmissionFiles("S-TEST4", "File4.txt")
+            fileList.delete()
+        }
+
+        @Test
+        fun `JSON submission with XSL file list`() {
+            val submission = jsonObj {
+                "accno" to "S-TEST5"
+                "attributes" to jsonArray({
+                    "name" to "Title"
+                    "value" to "Test Submission"
+                })
+                "section" to {
+                    "accno" to "SECT-001"
+                    "type" to "Study"
+                    "attributes" to jsonArray({
+                        "name" to "Title"
+                        "value" to "Root Section"
+                    }, {
+                        "name" to "File List"
+                        "value" to "FileList.xlsx"
+                    })
+                }
+            }.toString()
+
+            val fileList = excel("${tempFolder.root.absolutePath}/FileList.xlsx") {
+                sheet("page tab") {
+                    row {
+                        cell("Files")
+                        cell("GEN")
+                    }
+                    row {
+                        cell("File5.txt")
+                        cell("ABC")
+                    }
+                }
+            }
+
+            val response = webClient.submitSingle(submission, SubmissionFormat.JSON, listOf(fileList, tempFolder.createFile("File5.txt")))
+            assertThat(response).isSuccessful()
+            assertSubmissionFiles("S-TEST5", "File5.txt")
+            fileList.delete()
+        }
+
+        private fun assertSubmissionFiles(accNo: String, testFile: String) {
+            val fileListName = "FileList"
+            val createdSubmission = submissionRepository.getExtendedByAccNo(accNo)
+            val submissionFolderPath = "$basePath/submission/${createdSubmission.relPath}"
+
+            Assertions.assertThat(createdSubmission.section.fileListName).isEqualTo(fileListName)
+            Assertions.assertThat(createdSubmission.extendedSection.fileList).isEqualTo(
+                FileList(fileListName, listOf(File(testFile, attributes = listOf(Attribute("GEN", "ABC"))))))
+
+            Assertions.assertThat(Paths.get("$submissionFolderPath/Files/$testFile")).exists()
+
+            Assertions.assertThat(Paths.get("$submissionFolderPath/$fileListName.xml")).exists()
+            Assertions.assertThat(Paths.get("$submissionFolderPath/$fileListName.json")).exists()
+            Assertions.assertThat(Paths.get("$submissionFolderPath/$fileListName.pagetab.tsv")).exists()
+        }
+    }
+}
