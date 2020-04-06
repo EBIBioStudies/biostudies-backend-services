70fc682a1125f0f0d129396ef8c1aeca113b46bc
Juan Camilo Rada Mesa <jcamilorada@ebi.ac.uk>
2020-04-06 03:27:08 -0500

    added nio file size calculation

--- submission/persistence/src/main/kotlin/ac/uk/ebi/biostd/persistence/mapping/extended/from/ToDbFile.kt
+++ submission/persistence/src/main/kotlin/ac/uk/ebi/biostd/persistence/mapping/extended/from/ToDbFile.kt
@@ -5,8 +5,11 @@ import ac.uk.ebi.biostd.persistence.model.DbFile
 import ac.uk.ebi.biostd.persistence.model.DbFileAttribute
 import ebi.ac.uk.extended.model.ExtAttribute
 import ebi.ac.uk.extended.model.ExtFile
+import java.io.File
+import java.nio.file.Files
 
 internal fun ExtFile.toDbFile(order: Int, tableIndex: Int = NO_TABLE_INDEX) =
-    DbFile(fileName, order, file.length(), attributes.mapIndexedTo(sortedSetOf(), ::toDbFileAttribute), tableIndex)
+    DbFile(fileName, order, fileSize(file), attributes.mapIndexedTo(sortedSetOf(), ::toDbFileAttribute), tableIndex)
 
 private fun toDbFileAttribute(index: Int, attr: ExtAttribute) = DbFileAttribute(attr.toDbAttribute(index))
+private fun fileSize(file: File) = Files.size(file.toPath())
