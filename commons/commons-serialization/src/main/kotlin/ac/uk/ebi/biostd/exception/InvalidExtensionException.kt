7408a05f8697ed3881ec7a28249533316686dad2
Jhoan Manuel Muñoz Serrano <jhoanmanuelms@gmail.com>
2020-04-14 11:51:36 +0100

    Pivotal ID 172080341: File List Independant Format
    Improve error message

--- commons/commons-serialization/src/main/kotlin/ac/uk/ebi/biostd/exception/InvalidExtensionException.kt
+++ commons/commons-serialization/src/main/kotlin/ac/uk/ebi/biostd/exception/InvalidExtensionException.kt
@@ -2,5 +2,5 @@ package ac.uk.ebi.biostd.exception
 
 class InvalidExtensionException(private val ext: String) : RuntimeException() {
     override val message: String
-        get() = "Unsupported submission or file list format $ext"
+        get() = "Unsupported page tab format $ext"
 }
