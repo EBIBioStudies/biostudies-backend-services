cb290f94589bb26a104f82465782f6f02a90befb
Jhoan Manuel Muñoz Serrano <jhoanmanuelms@gmail.com>
2020-05-18 19:07:23 +0100

    Pivotal ID # 169759546: Add Number Of Views Metadata For Each Accession
    - Paginate the stats response 
    -  Validate that the submission exists before registering the stat

--- submission/submission-webapp/src/main/kotlin/ac/uk/ebi/biostd/stats/web/StatsResource.kt
+++ submission/submission-webapp/src/main/kotlin/ac/uk/ebi/biostd/stats/web/StatsResource.kt
@@ -1,16 +1,19 @@
 package ac.uk.ebi.biostd.stats.web
 
+import ac.uk.ebi.biostd.persistence.filter.PaginationFilter
 import ac.uk.ebi.biostd.stats.web.handlers.StatsFileHandler
 import ac.uk.ebi.biostd.submission.domain.service.TempFileGenerator
 import ebi.ac.uk.model.constants.MULTIPART_FORM_DATA
 import io.swagger.annotations.Api
 import io.swagger.annotations.ApiImplicitParam
+import io.swagger.annotations.ApiImplicitParams
 import io.swagger.annotations.ApiOperation
 import io.swagger.annotations.ApiParam
 import org.springframework.http.HttpHeaders.CONTENT_TYPE
 import org.springframework.http.MediaType.APPLICATION_JSON_VALUE
 import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.ModelAttribute
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -35,17 +38,33 @@ class StatsResource(
     @GetMapping("/{type}", produces = [APPLICATION_JSON_VALUE])
     @ResponseBody
     @ApiOperation("List all the registered submission stats of the given type")
-    @ApiImplicitParam(name = "X-SESSION-TOKEN", value = "Authentication token", required = true, paramType = "header")
+    @ApiImplicitParams(value = [
+        ApiImplicitParam(
+            name = "X-SESSION-TOKEN",
+            required = true,
+            paramType = "header",
+            value = "Authentication token"),
+        ApiImplicitParam(
+            name = "limit",
+            paramType = "query",
+            value = "Optional query parameter to set the maximum amount of stats in the response"),
+        ApiImplicitParam(
+            name = "offset",
+            paramType = "query",
+            value = "Optional query parameter to indicate from which stat should the response start")
+    ])
     fun findByType(
         @ApiParam(name = "type", value = "The type of stat. It should be one of the following: views")
-        @PathVariable type: String
-    ): List<SubmissionStat> = submissionStatsService.findByType(SubmissionStatType.valueOf(type.toUpperCase()))
+        @PathVariable type: String,
+
+        @ModelAttribute filter: PaginationFilter
+    ): List<SubmissionStat> = submissionStatsService.findByType(SubmissionStatType.valueOf(type.toUpperCase()), filter)
 
     @GetMapping("/{type}/{accNo}")
     @ResponseBody
     @ApiOperation("List all the registered submission stats of the given type for the given accession")
     @ApiImplicitParam(name = "X-SESSION-TOKEN", value = "Authentication token", required = true, paramType = "header")
-    fun findByType(
+    fun findByTypeAndAccNo(
         @ApiParam(name = "type", value = "The type of stat. It should be one of the following: views")
         @PathVariable type: String,
 
