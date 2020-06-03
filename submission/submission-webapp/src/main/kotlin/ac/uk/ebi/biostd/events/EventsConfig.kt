577386b3bf6ffe17ce6e9b55a95d6fc81da1818d
Juan Camilo Rada Mesa <jcamilorada@ebi.ac.uk>
2020-06-03 07:42:15 -0500

    code review changes

--- submission/submission-webapp/src/main/kotlin/ac/uk/ebi/biostd/events/EventsConfig.kt
+++ submission/submission-webapp/src/main/kotlin/ac/uk/ebi/biostd/events/EventsConfig.kt
@@ -2,7 +2,9 @@ package ac.uk.ebi.biostd.events
 
 import ac.uk.ebi.biostd.common.property.ApplicationProperties
 import org.springframework.amqp.core.TopicExchange
+import org.springframework.amqp.rabbit.connection.ConnectionFactory
 import org.springframework.amqp.rabbit.core.RabbitTemplate
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
@@ -11,6 +13,18 @@ const val PUBLISH_KEY = "bio.submission.published"
 
 @Configuration
 class EventsConfig {
+
+    @Bean
+    fun rabbitTemplate(connectionFactory: ConnectionFactory): RabbitTemplate {
+        val rabbitTemplate = RabbitTemplate(connectionFactory)
+        rabbitTemplate.setMessageConverter(producerJackson2MessageConverter())
+        return rabbitTemplate
+    }
+
+    @Bean
+    fun producerJackson2MessageConverter(): Jackson2JsonMessageConverter {
+        return Jackson2JsonMessageConverter()
+    }
 
     @Bean
     fun exchange(): TopicExchange = TopicExchange(BIOSTUDIES_EXCHANGE)
