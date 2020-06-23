113b49960cc4801c14c875157c35388556dfd62d
Jhoan Manuel Muñoz Serrano <jhoanmanuelms@gmail.com>
2020-06-23 15:50:52 +0100

    Pivotal ID # 173383085: Stats Performance Improvement (#230)
    - Reduce the number of database calls:
      - Summarize the increments before persisting them
      - Exclude unnecessary updates from persistence
    - Validate submission existence in updates and increments

--- submission/persistence/src/main/kotlin/ac/uk/ebi/biostd/persistence/exception/SubmissionNotFoundException.kt
+++ submission/persistence/src/main/kotlin/ac/uk/ebi/biostd/persistence/exception/SubmissionNotFoundException.kt
@@ -1,3 +1,7 @@
 package ac.uk.ebi.biostd.persistence.exception
 
 class SubmissionNotFoundException(accNo: String) : RuntimeException("The submission '$accNo' was not found")
+
+class SubmissionsNotFoundException(
+    accNo: List<String>
+) : RuntimeException("The following submissions were not found: ${accNo.joinToString(", ")}")
