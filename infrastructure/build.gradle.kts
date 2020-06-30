ed8ce49e3af708ef4bcc02dfb745eefd34df6cf5
Jhoan Manuel Muñoz Serrano <jhoanmanuelms@gmail.com>
2020-06-30 11:21:28 +0100

    Pivotal ID # 172817960: Resubmit Notification (#234)
    - Create a script to configure a RabbitMQ server
    - Optimize the gradle tasks and update it to use Kotlin DSL
    - Add missing properties to application.yml and application-local.yml
    - Improve documentation

---
+++ infrastructure/build.gradle.kts
@@ -0,0 +1,44 @@
+import java.util.Locale.ROOT
+
+tasks.register("setUpRabbitMQ") {
+    project.ext["args"] = ""
+    project.ext["linuxScript"] = "./SetUpRabbitMQ.sh"
+    project.ext["windowsScript"] = "SetUpRabbitMQ.cmd"
+    project.ext["workingDir"] = "./src/main/resources/setup/rabbitmq"
+
+    finalizedBy("executeScript")
+}
+
+tasks.register("setUpCleanDatabase") {
+    project.ext["args"] = "clean"
+
+    finalizedBy("setUpDatabase")
+}
+
+tasks.register("setUpTestDatabase") {
+    project.ext["args"] = "test"
+
+    finalizedBy("setUpDatabase")
+}
+
+tasks.register("setUpDatabase") {
+    project.ext["linuxScript"] = "./SetUpDatabase.sh"
+    project.ext["windowsScript"] = "SetUpDatabase.cmd"
+    project.ext["workingDir"] = "./src/main/resources/setup/database"
+
+    finalizedBy("executeScript")
+}
+
+tasks.register<Exec>("executeScript") {
+    workingDir = File(project.ext["workingDir"].toString())
+
+    val args = project.ext["args"].toString()
+    val linuxScript = project.ext["linuxScript"].toString()
+    val windowsScript = project.ext["windowsScript"].toString()
+    val os = System.getProperty("os.name").toLowerCase(ROOT)
+
+    commandLine = when {
+        os.contains("windows") -> mutableListOf("cmd", "/c", windowsScript, args)
+        else -> mutableListOf(linuxScript, args)
+    }
+}
