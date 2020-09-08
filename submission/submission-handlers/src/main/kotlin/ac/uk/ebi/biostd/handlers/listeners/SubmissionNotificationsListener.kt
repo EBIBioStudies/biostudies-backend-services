a3e172d7516218334cc33718cdc5e2ae1b14943c
Jhoan Manuel Muñoz Serrano <jhoanmanuelms@gmail.com>
2020-09-08 11:16:05 +0100

    Pivotal ID # 174621725: Wrong Notifications UI URL (#266)
    - Use the proper UI URL for the submission notifications
    - Use the application name instead of the port to kill the process
    - Remove unused code

--- submission/submission-handlers/src/main/kotlin/ac/uk/ebi/biostd/handlers/listeners/SubmissionNotificationsListener.kt
+++ submission/submission-handlers/src/main/kotlin/ac/uk/ebi/biostd/handlers/listeners/SubmissionNotificationsListener.kt
@@ -3,6 +3,7 @@ package ac.uk.ebi.biostd.handlers.listeners
 import ac.uk.ebi.biostd.handlers.config.NOTIFICATIONS_QUEUE
 import ac.uk.ebi.biostd.handlers.api.BioStudiesWebConsumer
 import ebi.ac.uk.extended.events.SubmissionSubmitted
+import ebi.ac.uk.notifications.integration.NotificationProperties
 import ebi.ac.uk.notifications.service.RtNotificationService
 import mu.KotlinLogging
 import org.springframework.amqp.rabbit.annotation.RabbitListener
@@ -11,7 +12,8 @@ private val logger = KotlinLogging.logger {}
 
 class SubmissionNotificationsListener(
     private val webConsumer: BioStudiesWebConsumer,
-    private val rtNotificationService: RtNotificationService
+    private val rtNotificationService: RtNotificationService,
+    private val notificationProperties: NotificationProperties
 ) {
     @RabbitListener(queues = [NOTIFICATIONS_QUEUE])
     fun receiveMessage(message: SubmissionSubmitted) {
@@ -20,7 +22,7 @@ class SubmissionNotificationsListener(
 
         if (extUser.notificationsEnabled) {
             val submission = webConsumer.getExtSubmission(message.extTabUrl)
-            rtNotificationService.notifySuccessfulSubmission(submission, extUser.fullName, message.uiUrl)
+            rtNotificationService.notifySuccessfulSubmission(submission, extUser.fullName, notificationProperties.uiUrl)
         }
     }
 }
