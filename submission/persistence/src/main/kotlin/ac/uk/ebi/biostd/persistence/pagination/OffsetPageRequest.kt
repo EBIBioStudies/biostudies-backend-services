57481511e95c7691c40a1ca0a4d3901cf769c96f
Juan Camilo Rada Mesa <jcamilorada@ebi.ac.uk>
2020-10-05 16:30:08 -0500

    added properties

--- submission/persistence/src/main/kotlin/ac/uk/ebi/biostd/persistence/pagination/OffsetPageRequest.kt
+++ submission/persistence/src/main/kotlin/ac/uk/ebi/biostd/persistence/pagination/OffsetPageRequest.kt
@@ -3,6 +3,7 @@ package ac.uk.ebi.biostd.persistence.pagination
 import org.springframework.data.domain.Pageable
 import org.springframework.data.domain.Sort
 
+@Suppress("ConstructorParameterNaming")
 class OffsetPageRequest(
     private val _offset: Long,
     private val _limit: Int,
@@ -16,5 +17,4 @@ class OffsetPageRequest(
     override fun previousOrFirst() = if (hasPrevious()) OffsetPageRequest(_offset - _limit, _limit, _sort) else first()
     override fun first(): Pageable = OffsetPageRequest(0, _limit, _sort)
     override fun hasPrevious(): Boolean = _offset > _limit
-
 }
