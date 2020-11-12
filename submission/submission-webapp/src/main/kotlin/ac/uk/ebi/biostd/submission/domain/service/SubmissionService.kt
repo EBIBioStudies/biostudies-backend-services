cfddfcc432c8c713a2d0e7928dceaebfa1a7e3cb
Jhoan Manuel Muñoz Serrano <jhoanmanuelms@gmail.com>
2020-11-12 10:11:00 +0000

    Pivotal ID # 175266840: Manual Set AccNo Sequence
    Add delay for testing

--- submission/submission-webapp/src/main/kotlin/ac/uk/ebi/biostd/submission/domain/service/SubmissionService.kt
+++ submission/submission-webapp/src/main/kotlin/ac/uk/ebi/biostd/submission/domain/service/SubmissionService.kt
@@ -58,6 +58,8 @@ class SubmissionService(
     @RabbitListener(queues = [SUBMISSION_REQUEST_QUEUE], concurrency = "1-1")
     fun processSubmission(request: SubmissionRequestMessage) {
         logger.info { "received process message for submission ${request.submission}" }
+        Thread.sleep(30_000L)
+
         val submission = submissionSubmitter.processRequest(SaveSubmissionRequest(request.submission, request.fileMode))
         eventsPublisherService.submissionSubmitted(submission)
     }
