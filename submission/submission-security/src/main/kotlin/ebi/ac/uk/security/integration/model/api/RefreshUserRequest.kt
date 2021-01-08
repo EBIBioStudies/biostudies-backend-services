1f31124cbed7428fa7f86a409c7c7e12552032f4
Juan Camilo Rada Mesa <jcamilorada@ebi.ac.uk>
2021-01-08 05:30:10 -0500

    code review changes

---
+++ submission/submission-security/src/main/kotlin/ebi/ac/uk/security/integration/model/api/RefreshUserRequest.kt
@@ -0,0 +1,3 @@
+package ebi.ac.uk.security.integration.model.api
+
+class RefreshUserRequest(val email: String)
--- submission/submission-webapp/src/main/kotlin/ac/uk/ebi/biostd/security/web/UserResource.kt
+++ submission/submission-webapp/src/main/kotlin/ac/uk/ebi/biostd/security/web/UserResource.kt
@@ -1,15 +1,16 @@
 package ac.uk.ebi.biostd.security.web
 
 import ebi.ac.uk.security.integration.components.ISecurityService
+import ebi.ac.uk.security.integration.model.api.RefreshUserRequest
 import ebi.ac.uk.security.integration.model.api.SecurityUser
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.ResponseBody
 
 @Controller
 class UserResource(val securityService: ISecurityService) {
     @PostMapping("/auth/refresh-user")
     @ResponseBody
-    fun refreshUser(@RequestParam("email") email: String): SecurityUser = securityService.refreshUser(email)
+    fun refreshUser(@RequestBody request: RefreshUserRequest): SecurityUser = securityService.refreshUser(request.email)
 }
