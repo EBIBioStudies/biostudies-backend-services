951cef9457c3d7f726b12cc328567b86d2fe287e
Juan Camilo Rada Mesa <jcamilorada@ebi.ac.uk>
2021-03-14 16:08:19 -0500

    Pivotal ID# 174948695: PTSubmit Delete Operation https://www.pivotaltracker.com/story/show/174948695

--- client/bio-commandline/src/main/kotlin/uk/ac/ebi/biostd/client/cli/BioStudiesCommandLine.kt
+++ client/bio-commandline/src/main/kotlin/uk/ac/ebi/biostd/client/cli/BioStudiesCommandLine.kt
@@ -1,83 +1,17 @@
 package uk.ac.ebi.biostd.client.cli
 
-import ac.uk.ebi.biostd.client.exception.SecurityWebClientException
-import ac.uk.ebi.biostd.client.exception.WebClientException
-import ac.uk.ebi.biostd.client.integration.web.SecurityWebClient
 import com.github.ajalt.clikt.core.CliktCommand
-import com.github.ajalt.clikt.core.PrintMessage
-import com.github.ajalt.clikt.parameters.options.option
-import com.github.ajalt.clikt.parameters.options.required
-import com.github.ajalt.clikt.parameters.types.file
-import java.io.File
-import java.io.StringReader
-import java.io.StringWriter
-import javax.xml.transform.OutputKeys
-import javax.xml.transform.TransformerFactory
-import javax.xml.transform.stream.StreamResult
-import javax.xml.transform.stream.StreamSource
+import com.github.ajalt.clikt.core.subcommands
+import uk.ac.ebi.biostd.client.cli.commands.DeleteCommand
+import uk.ac.ebi.biostd.client.cli.commands.SubmitCommand
+import uk.ac.ebi.biostd.client.cli.services.SubmissionService
 
-private const val FILES_SEPARATOR = ','
-
-class BioStudiesCommandLine : CliktCommand(name = "PTSubmit") {
-    private val server by option("-s", "--server", help = "BioStudies host url").required()
-    private val user by option("-u", "--user", help = "User that will perform the submission").required()
-    private val password by option("-p", "--password", help = "The user password").required()
-    private val onBehalf by option("-b", "--onBehalf", help = "Perform the operation on behalf of this user")
-    private val input by option(
-        "-i", "--input", help = "Path to the file containing the submission page tab").file(exists = true)
-    private val attached by option(
-        "-a", "--attached", help = "Comma separated list of paths to the files referenced in the submission")
-
-    @Suppress("TooGenericExceptionCaught")
+class BioStudiesCommandLine : CliktCommand() {
     override fun run() {
-        runCatching {
-            val files: MutableList<File> = mutableListOf()
-
-            attached?.let {
-                it.split(FILES_SEPARATOR).forEach { path -> addFiles(files, path) }
-            }
-
-            val client = getClient(server, user, password, onBehalf)
-            val submission = client.submitSingle(input!!, files).body
-
-            echo("SUCCESS: Submission with AccNo ${submission.accNo} was submitted")
-        }.onFailure {
-            val message = when (it) {
-                is WebClientException -> formatErrorMessage(it.message!!)
-                is SecurityWebClientException -> it.message!!
-                else -> it.message!!
-            }
-
-            throw PrintMessage(message)
-        }
-    }
-
-    internal fun getClient(host: String, user: String, password: String, onBehalf: String?) =
-        if (onBehalf.isNullOrBlank()) SecurityWebClient.create(host).getAuthenticatedClient(user, password)
-        else SecurityWebClient.create(host).getAuthenticatedClient(user, password, onBehalf)
-
-    private fun addFiles(files: MutableList<File>, path: String) {
-        val file = File(path)
-
-        if (file.isDirectory) file.walk().filter { it.isFile }.forEach { files.add(it) }
-        else files.add(file)
-    }
-
-    private fun formatErrorMessage(message: String): String {
-        val xmlOutput = StreamResult(StringWriter())
-        val xmlInput = StreamSource(StringReader(message))
-        val transformer =
-            TransformerFactory
-                .newInstance()
-                .apply { setAttribute("indent-number", 2) }
-                .newTransformer()
-
-        transformer.setOutputProperty(OutputKeys.INDENT, "yes")
-        transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes")
-        transformer.transform(xmlInput, xmlOutput)
-
-        return xmlOutput.writer.toString()
+        echo("That's BioStudies command line")
     }
 }
 
-fun main(args: Array<String>) = BioStudiesCommandLine().main(args)
+fun main(args: Array<String>) = BioStudiesCommandLine()
+    .subcommands(SubmitCommand(SubmissionService()), DeleteCommand(SubmissionService()))
+    .main(args)
--- client/bio-commandline/src/test/kotlin/uk/ac/ebi/biostd/client/cli/BioStudiesCommandLineTest.kt
+++ client/bio-commandline/src/test/kotlin/uk/ac/ebi/biostd/client/cli/BioStudiesCommandLineTest.kt
@@ -1,138 +1,139 @@
-package uk.ac.ebi.biostd.client.cli
-
-import ac.uk.ebi.biostd.client.exception.WebClientException
-import ac.uk.ebi.biostd.client.integration.web.BioWebClient
-import ac.uk.ebi.biostd.client.integration.web.SubmissionResponse
-import com.github.ajalt.clikt.core.IncorrectOptionValueCount
-import com.github.ajalt.clikt.core.MissingParameter
-import com.github.ajalt.clikt.core.PrintMessage
-import ebi.ac.uk.model.Submission
-import io.github.glytching.junit.extension.folder.TemporaryFolder
-import io.github.glytching.junit.extension.folder.TemporaryFolderExtension
-import io.mockk.clearAllMocks
-import io.mockk.every
-import io.mockk.impl.annotations.MockK
-import io.mockk.impl.annotations.SpyK
-import io.mockk.junit5.MockKExtension
-import io.mockk.verify
-import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.AfterEach
-import org.junit.jupiter.api.BeforeEach
-import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.assertThrows
-import org.junit.jupiter.api.extension.ExtendWith
-import org.springframework.http.HttpStatus.BAD_REQUEST
-import java.io.File
-
-@ExtendWith(MockKExtension::class, TemporaryFolderExtension::class)
-class BioStudiesCommandLineTest(
-    private val temporaryFolder: TemporaryFolder,
-    @MockK private val mockWebClient: BioWebClient
-) {
-    @SpyK
-    private var testInstance = BioStudiesCommandLine()
-
-    private lateinit var rootFolder: String
-    private lateinit var submissionFile: File
-
-    @BeforeEach
-    fun setUp() {
-        rootFolder = temporaryFolder.root.absolutePath
-        submissionFile = temporaryFolder.createFile("Submission.tsv")
-
-        temporaryFolder.createDirectory("attachments")
-        temporaryFolder.createDirectory("attachments/inner")
-
-        val mockResponse = SubmissionResponse.ok(Submission("S-TEST123"))
-        val libFile = temporaryFolder.createFile("FileList.tsv")
-        val refFile = temporaryFolder.createFile("attachments/inner/RefFile.txt")
-
-        every { mockWebClient.submitSingle(submissionFile, listOf()) } returns mockResponse
-        every { mockWebClient.submitSingle(submissionFile, listOf(libFile, refFile)) } returns mockResponse
-        every { testInstance.getClient("http://localhost:8080", "user", "123456", null) } returns mockWebClient
-        every { testInstance.getClient("http://localhost:8080", "user", "123456", "other") } returns mockWebClient
-    }
-
-    @AfterEach
-    fun tearDown() = clearAllMocks()
-
-    @Test
-    fun `submit single with no attachments`() {
-        val args = arrayOf(
-            "-s", "http://localhost:8080",
-            "-u", "user",
-            "-p", "123456",
-            "-i", "$rootFolder/Submission.tsv")
-
-        testInstance.main(args)
-        verify(exactly = 1) { testInstance.getClient("http://localhost:8080", "user", "123456", null) }
-    }
-
-    @Test
-    fun `submit single on behalf`() {
-        val args = arrayOf(
-            "-s", "http://localhost:8080",
-            "-u", "user",
-            "-p", "123456",
-            "-b", "other",
-            "-i", "$rootFolder/Submission.tsv")
-
-        testInstance.main(args)
-        verify(exactly = 1) { testInstance.getClient("http://localhost:8080", "user", "123456", "other") }
-    }
-
-    @Test
-    fun `submit single with attachments`() {
-        val args = arrayOf(
-            "-s", "http://localhost:8080",
-            "-u", "user",
-            "-p", "123456",
-            "-i", "$rootFolder/Submission.tsv",
-            "-a", "$rootFolder/FileList.tsv,$rootFolder/attachments")
-
-        testInstance.main(args)
-    }
-
-    @Test
-    fun `submit single with invalid files`() {
-        val args = arrayOf(
-            "-s", "http://localhost:8080",
-            "-u", "user",
-            "-p", "123456",
-            "-i", "$rootFolder/Submission.tsv")
-
-        every {
-            mockWebClient.submitSingle(submissionFile, listOf())
-        } throws WebClientException(BAD_REQUEST, "<ValidationErrors><error>Invalid Files</error></ValidationErrors>")
-
-        val exception = assertThrows<PrintMessage> { testInstance.parse(args) }
-        assertThat(exception.message).isEqualTo(
-            "<ValidationErrors>\n  <error>Invalid Files</error>\n</ValidationErrors>\n")
-    }
-
-    @Test
-    fun `submit single with generic exception`() {
-        val args = arrayOf(
-            "-s", "http://localhost:8080",
-            "-u", "user",
-            "-p", "123456",
-            "-i", "$rootFolder/Submission.tsv")
-
-        every { mockWebClient.submitSingle(submissionFile, listOf()) } throws Exception("Generic Exception")
-
-        val exception = assertThrows<Exception> { testInstance.parse(args) }
-        assertThat(exception.message).isEqualTo("Generic Exception")
-    }
-
-    @Test
-    fun `missing arguments`() {
-        val exceptionMessage = assertThrows<MissingParameter> { testInstance.parse(arrayOf("-u", "manager")) }.message
-        assertThat(exceptionMessage).isEqualTo("Missing option \"--server\".")
-    }
-
-    @Test
-    fun `missing value for argument`() {
-        val exceptionMessage = assertThrows<IncorrectOptionValueCount> { testInstance.parse(arrayOf("-u")) }.message
-        assertThat(exceptionMessage).isEqualTo("-u option requires an argument")
-    }
-}
+// package uk.ac.ebi.biostd.client.cli
+//
+// import ac.uk.ebi.biostd.client.exception.WebClientException
+// import ac.uk.ebi.biostd.client.integration.web.BioWebClient
+// import ac.uk.ebi.biostd.client.integration.web.SubmissionResponse
+// import com.github.ajalt.clikt.core.IncorrectOptionValueCount
+// import com.github.ajalt.clikt.core.MissingParameter
+// import com.github.ajalt.clikt.core.PrintMessage
+// import ebi.ac.uk.model.Submission
+// import io.github.glytching.junit.extension.folder.TemporaryFolder
+// import io.github.glytching.junit.extension.folder.TemporaryFolderExtension
+// import io.mockk.clearAllMocks
+// import io.mockk.every
+// import io.mockk.impl.annotations.MockK
+// import io.mockk.impl.annotations.SpyK
+// import io.mockk.junit5.MockKExtension
+// import io.mockk.verify
+// import org.assertj.core.api.Assertions.assertThat
+// import org.junit.jupiter.api.AfterEach
+// import org.junit.jupiter.api.BeforeEach
+// import org.junit.jupiter.api.Test
+// import org.junit.jupiter.api.assertThrows
+// import org.junit.jupiter.api.extension.ExtendWith
+// import org.springframework.http.HttpStatus.BAD_REQUEST
+// import uk.ac.ebi.biostd.client.cli.services.SubmissionService
+// import java.io.File
+//
+// @ExtendWith(MockKExtension::class, TemporaryFolderExtension::class)
+// class BioStudiesCommandLineTest(
+//    private val temporaryFolder: TemporaryFolder,
+//    @MockK private val mockWebClient: BioWebClient
+// ) {
+//    @SpyK
+//    private var testInstance = Submit(SubmissionService())
+//
+//    private lateinit var rootFolder: String
+//    private lateinit var submissionFile: File
+//
+//    @BeforeEach
+//    fun setUp() {
+//        rootFolder = temporaryFolder.root.absolutePath
+//        submissionFile = temporaryFolder.createFile("Submission.tsv")
+//
+//        temporaryFolder.createDirectory("attachments")
+//        temporaryFolder.createDirectory("attachments/inner")
+//
+//        val mockResponse = SubmissionResponse.ok(Submission("S-TEST123"))
+//        val libFile = temporaryFolder.createFile("FileList.tsv")
+//        val refFile = temporaryFolder.createFile("attachments/inner/RefFile.txt")
+//
+//        every { mockWebClient.submitSingle(submissionFile, listOf()) } returns mockResponse
+//        every { mockWebClient.submitSingle(submissionFile, listOf(libFile, refFile)) } returns mockResponse
+//        every { getClient("http://localhost:8080", "user", "123456", null) } returns mockWebClient
+//        every { getClient("http://localhost:8080", "user", "123456", "other") } returns mockWebClient
+//    }
+//
+//    @AfterEach
+//    fun tearDown() = clearAllMocks()
+//
+//    @Test
+//    fun `submit single with no attachments`() {
+//        val args = arrayOf(
+//            "-s", "http://localhost:8080",
+//            "-u", "user",
+//            "-p", "123456",
+//            "-i", "$rootFolder/Submission.tsv")
+//
+//        testInstance.main(args)
+//        verify(exactly = 1) { getClient("http://localhost:8080", "user", "123456", null) }
+//    }
+//
+//    @Test
+//    fun `submit single on behalf`() {
+//        val args = arrayOf(
+//            "-s", "http://localhost:8080",
+//            "-u", "user",
+//            "-p", "123456",
+//            "-b", "other",
+//            "-i", "$rootFolder/Submission.tsv")
+//
+//        testInstance.main(args)
+//        verify(exactly = 1) { getClient("http://localhost:8080", "user", "123456", "other") }
+//    }
+//
+//    @Test
+//    fun `submit single with attachments`() {
+//        val args = arrayOf(
+//            "-s", "http://localhost:8080",
+//            "-u", "user",
+//            "-p", "123456",
+//            "-i", "$rootFolder/Submission.tsv",
+//            "-a", "$rootFolder/FileList.tsv,$rootFolder/attachments")
+//
+//        testInstance.main(args)
+//    }
+//
+//    @Test
+//    fun `submit single with invalid files`() {
+//        val args = arrayOf(
+//            "-s", "http://localhost:8080",
+//            "-u", "user",
+//            "-p", "123456",
+//            "-i", "$rootFolder/Submission.tsv")
+//
+//        every {
+//            mockWebClient.submitSingle(submissionFile, listOf())
+//        } throws WebClientException(BAD_REQUEST, "<ValidationErrors><error>Invalid Files</error></ValidationErrors>")
+//
+//        val exception = assertThrows<PrintMessage> { testInstance.parse(args) }
+//        assertThat(exception.message).isEqualTo(
+//            "<ValidationErrors>\n  <error>Invalid Files</error>\n</ValidationErrors>\n")
+//    }
+//
+//    @Test
+//    fun `submit single with generic exception`() {
+//        val args = arrayOf(
+//            "-s", "http://localhost:8080",
+//            "-u", "user",
+//            "-p", "123456",
+//            "-i", "$rootFolder/Submission.tsv")
+//
+//        every { mockWebClient.submitSingle(submissionFile, listOf()) } throws Exception("Generic Exception")
+//
+//        val exception = assertThrows<Exception> { testInstance.parse(args) }
+//        assertThat(exception.message).isEqualTo("Generic Exception")
+//    }
+//
+//    @Test
+//    fun `missing arguments`() {
+//        val exceptionMessage = assertThrows<MissingParameter> { testInstance.parse(arrayOf("-u", "manager")) }.message
+//        assertThat(exceptionMessage).isEqualTo("Missing option \"--server\".")
+//    }
+//
+//    @Test
+//    fun `missing value for argument`() {
+//        val exceptionMessage = assertThrows<IncorrectOptionValueCount> { testInstance.parse(arrayOf("-u")) }.message
+//        assertThat(exceptionMessage).isEqualTo("-u option requires an argument")
+//    }
+// }
