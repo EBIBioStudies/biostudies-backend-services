7e32f989d1f16405604eb4ef9bf36e42ac791958
Jhoan Manuel Muñoz Serrano <jhoanmanuelms@gmail.com>
2021-03-29 17:25:47 +0100

    Pivotal ID # 177304040: PT Submit Migration Operation
    Create an operation in PT submit to migrate a submission from one environment to another

---
+++ client/bio-commandline/src/main/kotlin/uk/ac/ebi/biostd/client/cli/dto/MigrationRequest.kt
@@ -0,0 +1,11 @@
+package uk.ac.ebi.biostd.client.cli.dto
+
+data class MigrationRequest(
+    val accNo: String,
+    val source: String,
+    val sourceUser: String,
+    val sourcePassword: String,
+    val target: String,
+    val targetUser: String,
+    val targetPassword: String
+)
--- client/bio-commandline/src/main/kotlin/uk/ac/ebi/biostd/client/cli/services/SubmissionService.kt
+++ client/bio-commandline/src/main/kotlin/uk/ac/ebi/biostd/client/cli/services/SubmissionService.kt
@@ -4,41 +4,36 @@ import ac.uk.ebi.biostd.client.integration.web.SecurityWebClient
 import com.github.ajalt.clikt.core.PrintMessage
 import ebi.ac.uk.model.Submission
 import org.apache.commons.lang3.exception.ExceptionUtils
-import java.io.File
-
-data class SubmissionRequest(
-    val server: String,
-    val user: String,
-    val password: String,
-    val onBehalf: String?,
-    val file: File,
-    val attached: List<File>
-)
-
-data class SubmissionDeleteRequest(
-    val server: String,
-    val user: String,
-    val password: String,
-    val onBehalf: String?,
-    val accNo: String
-)
+import uk.ac.ebi.biostd.client.cli.dto.DeletionRequest
+import uk.ac.ebi.biostd.client.cli.dto.MigrationRequest
+import uk.ac.ebi.biostd.client.cli.dto.SubmissionRequest
 
 class SubmissionService {
-    fun submit(request: SubmissionRequest): Submission {
-        return runCatching { submitRequest(request) }.getOrElse { throw PrintMessage(ExceptionUtils.getMessage(it)) }
+    fun submit(request: SubmissionRequest): Submission = performRequest { submitRequest(request) }
+
+    fun delete(request: DeletionRequest) = performRequest { deleteRequest(request) }
+
+    fun migrate(request: MigrationRequest) = performRequest { migrateRequest(request) }
+
+    private fun deleteRequest(request: DeletionRequest) =
+        bioWebClient(request.server, request.user, request.password).deleteSubmission(request.accNo)
+
+    private fun submitRequest(request: SubmissionRequest): Submission =
+        bioWebClient(request.server, request.user, request.password).submitSingle(request.file, request.attached).body
+
+    private fun migrateRequest(request: MigrationRequest) {
+        val sourceClient = bioWebClient(request.source, request.sourceUser, request.sourcePassword)
+        val targetClient = bioWebClient(request.target, request.targetUser, request.targetPassword)
+
+        targetClient.submitExt(sourceClient.getExtByAccNo(request.accNo))
     }
 
-    fun delete(request: SubmissionDeleteRequest) {
-        return runCatching { deleteRequest(request) }.getOrElse { throw PrintMessage(ExceptionUtils.getMessage(it)) }
-    }
+    private inline fun <T> performRequest(request: () -> T) =
+        runCatching { request() }
+        .getOrElse { throw PrintMessage(ExceptionUtils.getMessage(it)) }
 
-    private fun deleteRequest(request: SubmissionDeleteRequest) {
-        val client = SecurityWebClient.create(request.server).getAuthenticatedClient(request.user, request.password)
-        return client.deleteSubmission(request.accNo)
-    }
-
-    private fun submitRequest(request: SubmissionRequest): Submission {
-        val client = SecurityWebClient.create(request.server).getAuthenticatedClient(request.user, request.password)
-        return client.submitSingle(request.file, request.attached).body
-    }
+    private fun bioWebClient(server: String, user: String, password: String) =
+        SecurityWebClient
+            .create(server)
+            .getAuthenticatedClient(user, password)
 }
