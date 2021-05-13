540e0e057fde6d100a43203b78f7bc0be3ea220b
Juan Camilo Rada Mesa <jcamilorada@ebi.ac.uk>
2021-05-13 18:24:35 -0500

    code review changes

--- submission/persistence-mongo/src/main/kotlin/ac/uk/ebi/biostd/persistence/doc/commons/MongockTemplateExt.kt
+++ submission/persistence-mongo/src/main/kotlin/ac/uk/ebi/biostd/persistence/doc/commons/MongockTemplateExt.kt
@@ -6,7 +6,7 @@ import com.github.cloudyrock.mongock.driver.mongodb.springdata.v3.decorator.impl
  * Ensure that a collection for the provided clazz exists. If not it will be created.
  *
  * @param clazz must not be null.
- * **/
+ */
 fun <T> MongockTemplate.ensureExists(clazz: Class<T>) {
     if (collectionExists(clazz).not()) createCollection(clazz)
 }
