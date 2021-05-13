57c084ba218bf6a0f2764e0f748cd518c19357bc
Juan Camilo Rada Mesa <jcamilorada@ebi.ac.uk>
2021-05-13 11:28:48 -0500

    spyk Test

--- submission/persistence-mongo/src/test/kotlin/ac/uk/ebi/biostd/persistence/doc/commons/MongockTemplateExtTest.kt
+++ submission/persistence-mongo/src/test/kotlin/ac/uk/ebi/biostd/persistence/doc/commons/MongockTemplateExtTest.kt
@@ -1,39 +1,26 @@
 package ac.uk.ebi.biostd.persistence.doc.commons
 
-import ac.uk.ebi.biostd.common.properties.ApplicationProperties
-import ac.uk.ebi.biostd.integration.SerializationConfig
-import ac.uk.ebi.biostd.integration.SerializationService
-import ac.uk.ebi.biostd.persistence.doc.commons.MongockTemplateExtTest.TestConfigMongock
-import ac.uk.ebi.biostd.persistence.doc.integration.MongoDbServicesConfig
 import ac.uk.ebi.biostd.persistence.doc.model.DocSubmission
-import com.github.cloudyrock.mongock.driver.api.lock.LockManager
-import com.github.cloudyrock.mongock.driver.api.lock.guard.invoker.LockGuardInvoker
 import com.github.cloudyrock.mongock.driver.api.lock.guard.invoker.LockGuardInvokerImpl
 import com.github.cloudyrock.mongock.driver.core.lock.DefaultLockManager
 import com.github.cloudyrock.mongock.driver.mongodb.springdata.v3.decorator.impl.MongockTemplate
 import com.github.cloudyrock.mongock.driver.mongodb.sync.v4.repository.MongoSync4LockRepository
 import com.github.cloudyrock.mongock.utils.TimeService
+import io.mockk.impl.annotations.SpyK
 import io.mockk.junit5.MockKExtension
+import io.mockk.spyk
+import io.mockk.verify
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
-import org.mockito.Mock
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.Configuration
-import org.springframework.context.annotation.Import
-import org.springframework.data.mongodb.core.MongoTemplate
 import org.springframework.data.mongodb.core.collectionExists
 import org.springframework.data.mongodb.core.createCollection
-import uk.ac.ebi.extended.serialization.service.ExtSerializationService
-import java.nio.file.Files
 
 @ExtendWith(MockKExtension::class)
-@Import(TestConfigMongock::class)
-class MongockTemplateExtTest(
-        @Autowired private val mongockTemplate: MongockTemplate
-) {
+class MongockTemplateExtTest {
+    val mongockTemplate = spyk<MongockTemplate>()
+
     @BeforeEach
     fun init() {
         mongockTemplate.collectionNames.toList().forEach { mongockTemplate.dropCollection(it) }
@@ -46,6 +33,8 @@ class MongockTemplateExtTest(
         mongockTemplate.ensureExists(DocSubmission::class.java)
 
         assertThat(mongockTemplate.collectionExists<DocSubmission>()).isTrue()
+        verify(exactly = 1) { mongockTemplate.collectionExists<DocSubmission>() }
+        verify(exactly = 1) { mongockTemplate.createCollection<DocSubmission>() }
     }
 
     @Test
@@ -59,15 +48,7 @@ class MongockTemplateExtTest(
 
         assertThat(mongockTemplate.collectionExists<DocSubmission>()).isTrue()
         assertThat(mongockTemplate.collectionNames.size).isEqualTo(1)
-    }
-
-    @Configuration
-    class TestConfigMongock(@Autowired private val mongoTemplate: MongoTemplate) {
-        @Bean
-        fun mongockTemplate() = MongockTemplate(mongoTemplate,
-                LockGuardInvokerImpl(DefaultLockManager(MongoSync4LockRepository(
-                        mongoTemplate.createCollection(DocSubmission::class.java),
-                        true),
-                        TimeService())))
+        verify(exactly = 1) { mongockTemplate.collectionExists<DocSubmission>() }
+        verify(exactly = 0) { mongockTemplate.createCollection<DocSubmission>() }
     }
 }
