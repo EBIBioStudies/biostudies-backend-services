09d86b38d3cdd0582b155736f16cf58258085094
Jhoan Manuel Muñoz Serrano <jhoanmanuelms@gmail.com>
2021-05-25 16:48:18 +0100

    Pivotal ID # 172336299: Remove Access Tags From Public Export (#374)
    * Pivotal ID # 172336299: Remove Access Tags From Public Export
    
    - Create the exporter service to export the public studies
    - Add utilities to trigger it daily or by demand
    - Allow disk usage for Mongo queries
    - Update MySQL connector version

---
+++ scheduler/task-properties/src/test/kotlin/ac/uk/ebi/scheduler/properties/ExporterPropertiesTest.kt
@@ -0,0 +1,28 @@
+package ac.uk.ebi.scheduler.properties
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class ExporterPropertiesTest {
+    @Test
+    fun `as java command`() {
+        val properties = ExporterProperties.create(
+            fileName = "publicOnlyStudies",
+            outputPath = "/an/output/path",
+            bioStudiesUrl = "http://localhost:8080",
+            bioStudiesUser = "admin_user@ebi.ac.uk",
+            bioStudiesPassword = "123456"
+        )
+
+        assertThat(properties.asJavaCommand("/apps-folder")).isEqualTo(
+            """
+            java -Dsun.jnu.encoding=UTF-8 -Xmx6g -jar /apps-folder/exporter-task-1.0.0.jar \
+            --app.fileName=publicOnlyStudies \
+            --app.outputPath=/an/output/path \
+            --app.bioStudies.url=http://localhost:8080 \
+            --app.bioStudies.user=admin_user@ebi.ac.uk \
+            --app.bioStudies.password=123456
+            """.trimIndent()
+        )
+    }
+}
