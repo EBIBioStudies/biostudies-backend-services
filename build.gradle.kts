53d15446d9ee61ca4f040ffe57628ada156596b4
Jhoan Manuel Muñoz Serrano <jhoanmanuelms@gmail.com>
2021-06-30 16:38:11 +0100

    Pivotal ID # 178375625: Actuator Git Commit
    Include git properties in the JAR file

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,6 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("com.gorylenko.gradle-git-properties") version "2.3.1"
     id("io.gitlab.arturbosch.detekt") version "1.16.0"
     id("org.jetbrains.kotlin.jvm") version "1.4.32"
     id("org.jlleitschuh.gradle.ktlint") version "10.0.0"
