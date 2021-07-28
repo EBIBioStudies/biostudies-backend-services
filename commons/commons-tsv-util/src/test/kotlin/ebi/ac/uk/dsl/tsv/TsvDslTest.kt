7663e32f5c3772c3556f9e459b9faff26ca0bde2
Juan Camilo Rada Mesa <jcamilorada@ebi.ac.uk>
2021-07-28 16:33:27 -0500

    generic tsvTest

--- commons/commons-tsv-util/src/test/kotlin/ebi/ac/uk/dsl/tsv/TsvDslTest.kt
+++ commons/commons-tsv-util/src/test/kotlin/ebi/ac/uk/dsl/tsv/TsvDslTest.kt
@@ -4,14 +4,14 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 val tsvTest = tsv {
-    line("Submission")
-    line("Title", "Excel Submission")
+    line("That's")
+    line("tsv", "test")
     line()
 }
 
 internal class TsvDslTest {
     @Test
     fun toStringTest() {
-        assertThat(tsvTest.toString()).isEqualTo("Submission\nTitle\tExcel Submission\n")
+        assertThat(tsvTest.toString()).isEqualTo("That's\ntsv\ttest\n")
     }
 }
