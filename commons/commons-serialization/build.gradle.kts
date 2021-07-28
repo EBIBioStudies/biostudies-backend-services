f459186b067e9dbf694912bd113d08f5927b63f6
Juan Camilo Rada Mesa <jcamilorada@ebi.ac.uk>
2021-07-27 21:21:48 -0500

    green bui9ld after refactor

--- commons/commons-serialization/build.gradle.kts
+++ commons/commons-serialization/build.gradle.kts
@@ -18,6 +18,7 @@ dependencies {
     api(project(":commons:commons-serialization-util"))
     api(project(":commons:commons-test"))
     api(project(":commons:commons-util"))
+    api(project(":commons:commons-tsv-util"))
 
     implementation(KotlinReflect)
     implementation(KotlinStdLib)
