fb57f1b5e01573e11e687cba686320c93ac4d57a
Juan Camilo Rada Mesa <jcamilorada@ebi.ac.uk>
2021-11-23 11:42:04 +0000

    code review changes

--- commons/commons-model-extended/src/test/kotlin/ebi/ac/uk/extended/model/ExtSubmissionExtensionsTest.kt
+++ commons/commons-model-extended/src/test/kotlin/ebi/ac/uk/extended/model/ExtSubmissionExtensionsTest.kt
@@ -17,29 +17,27 @@ class ExtSubmissionExtensionsTest {
         assertThat(submissionNoTitleNoSecTitle.description).isNull()
     }
 
-    private fun testSubmission(subTitle: String? = null, secTitle: String? = null): ExtSubmission {
-        return ExtSubmission(
-            accNo = "S-TEST1",
-            version = 1,
-            schemaVersion = "1.0",
-            owner = "owner@mail.org",
-            storageMode = StorageMode.FIRE,
-            submitter = "submitter@mail.org",
-            title = subTitle,
-            method = ExtSubmissionMethod.PAGE_TAB,
-            relPath = "/a/rel/path",
-            rootPath = null,
-            releaseTime = null,
-            released = true,
-            secretKey = "a-secret-key",
-            status = ExtProcessingStatus.PROCESSED,
-            modificationTime = OffsetDateTime.of(2020, 9, 21, 10, 30, 34, 15, ZoneOffset.UTC),
-            creationTime = OffsetDateTime.of(2020, 9, 21, 10, 30, 34, 15, ZoneOffset.UTC),
-            attributes = listOf(),
-            section = ExtSection(
-                type = "Study",
-                attributes = secTitle?.let { listOf(ExtAttribute("Title", it)) } ?: listOf()
-            )
+    private fun testSubmission(subTitle: String? = null, secTitle: String? = null): ExtSubmission = ExtSubmission(
+        accNo = "S-TEST1",
+        version = 1,
+        schemaVersion = "1.0",
+        owner = "owner@mail.org",
+        storageMode = StorageMode.FIRE,
+        submitter = "submitter@mail.org",
+        title = subTitle,
+        method = ExtSubmissionMethod.PAGE_TAB,
+        relPath = "/a/rel/path",
+        rootPath = null,
+        releaseTime = null,
+        released = true,
+        secretKey = "a-secret-key",
+        status = ExtProcessingStatus.PROCESSED,
+        modificationTime = OffsetDateTime.of(2020, 9, 21, 10, 30, 34, 15, ZoneOffset.UTC),
+        creationTime = OffsetDateTime.of(2020, 9, 21, 10, 30, 34, 15, ZoneOffset.UTC),
+        attributes = listOf(),
+        section = ExtSection(
+            type = "Study",
+            attributes = secTitle?.let { listOf(ExtAttribute("Title", it)) } ?: listOf()
         )
-    }
+    )
 }
