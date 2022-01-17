eeebc57c8013ef9795b1e898ab5c8e643f40a31d
Juan <jcamilorada@ebi.ac.uk>
2022-01-17 18:51:26 +0000

    Pivotal ID # 180929480: Allow submission request to be manually re trigger (#493)
    https://www.pivotaltracker.com/story/show/180929480

--- submission/submission-webapp/src/main/kotlin/ac/uk/ebi/biostd/submission/web/resources/ext/ExtSubmissionResource.kt
+++ submission/submission-webapp/src/main/kotlin/ac/uk/ebi/biostd/submission/web/resources/ext/ExtSubmissionResource.kt
@@ -48,6 +48,12 @@ class ExtSubmissionResource(
         @PathVariable accNo: String
     ): ExtSubmission = extSubmissionService.refreshSubmission(accNo, user.email)
 
+    @PostMapping("/re-trigger/{accNo}/{version}")
+    fun reTriggerSubmission(
+        @PathVariable accNo: String,
+        @PathVariable version: Int
+    ): ExtSubmission = extSubmissionService.reTriggerSubmission(accNo, version)
+
     @PostMapping
     @PreAuthorize("isAuthenticated()")
     fun submitExtended(
