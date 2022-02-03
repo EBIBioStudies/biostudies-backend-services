a94b55caead684e16310a6de29db8a0673e6a80d
liviuba <anita.liviu@gmail.com>
2022-02-03 21:30:40 +0200

    Pivotal ID # 181127806: Enable rabbitmq_web_stomp in setup script
    https://www.pivotaltracker.com/story/show/181127806

--- infrastructure/src/main/resources/setup/rabbitmq/SetUpRabbitMQ.cmd
+++ infrastructure/src/main/resources/setup/rabbitmq/SetUpRabbitMQ.cmd
@@ -3,5 +3,7 @@ docker run -d ^
   --name biostudies-rabbitmq ^
   -e RABBITMQ_DEFAULT_USER=manager ^
   -e RABBITMQ_DEFAULT_PASS=manager-local ^
-  -p 4369:4369 -p 5672:5672 -p 15672:15672 -p 25672:25672 ^
+  -p 4369:4369 -p 5672:5672 -p 15672:15672 -p 25672:25672 -p 15674:15674 ^
   rabbitmq:3-management
+
+docker exec biostudies-rabbitmq rabbitmq-plugins enable rabbitmq_web_stomp
