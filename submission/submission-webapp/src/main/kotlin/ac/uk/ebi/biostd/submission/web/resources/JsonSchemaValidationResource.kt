44acdf24b5c2b8b1c19b4b8290c3e91d864663c1
Kola <kola@ebi.ac.uk>
2022-04-30 18:30:51 +0100

    Allow use of custom schema in post request

--- submission/submission-webapp/src/main/kotlin/ac/uk/ebi/biostd/submission/web/resources/JsonSchemaValidationResource.kt
+++ submission/submission-webapp/src/main/kotlin/ac/uk/ebi/biostd/submission/web/resources/JsonSchemaValidationResource.kt
@@ -1,29 +1,89 @@
 package ac.uk.ebi.biostd.submission.web.resources
 
+import ac.uk.ebi.biostd.integration.SerializationConfig
+import ac.uk.ebi.biostd.integration.SerializationService
+import ac.uk.ebi.biostd.integration.SubFormat
 import ac.uk.ebi.biostd.submission.domain.service.JsonSchemaValidationService
+import com.mysql.cj.x.protobuf.MysqlxDatatypes
 import ebi.ac.uk.model.constants.APPLICATION_JSON
+import ebi.ac.uk.model.constants.SUBMISSION_TYPE
+import ebi.ac.uk.model.constants.TEXT_PLAIN
 import net.pwall.json.schema.output.BasicOutput
 import org.springframework.http.MediaType.APPLICATION_JSON_VALUE
 import org.springframework.web.bind.annotation.*
+import org.springframework.web.multipart.MultipartFile
+import java.io.File
 
 @RestController
 @RequestMapping("/jsonschema-validation")
 class JsonSchemaValidationResource {
     private val jsonSchemaValidationService: JsonSchemaValidationService
+    private val serializationService = SerializationConfig.serializationService()
 
     constructor(jsonSchemaValidationService: JsonSchemaValidationService) {
         this.jsonSchemaValidationService = jsonSchemaValidationService
     }
 
     @PostMapping(
+        headers = ["$SUBMISSION_TYPE=$APPLICATION_JSON"],
         produces = [APPLICATION_JSON_VALUE]
     )
-    fun validate(
+    fun validateJson(
         @RequestBody toValidate: String
     ) : BasicOutput {
-        val output = jsonSchemaValidationService.validateBasic(toValidate)
+        return validate(toValidate)
+    }
 
+    @PostMapping(
+        headers = ["$SUBMISSION_TYPE=$TEXT_PLAIN"],
+        produces = [APPLICATION_JSON_VALUE]
+    )
+    fun validateTsv(
+        @RequestBody toValidate: String
+    ) : BasicOutput {
+        val submission = serializationService.deserializeSubmission(toValidate, SubFormat.TSV)
+        val toValidateJson = serializationService.serializeSubmission(submission, SubFormat.JSON)
+        return validate(toValidateJson)
+    }
+
+    @PostMapping(
+        path = ["custom-schema"],
+        headers = ["$SUBMISSION_TYPE=$APPLICATION_JSON"],
+        produces = [APPLICATION_JSON_VALUE]
+    )
+    fun validateJsonCustom(
+        @RequestBody toValidate: String,
+        @RequestParam("schema") schema: MultipartFile
+    ) : BasicOutput {
+        val schemaContents = String(schema.bytes)
+        System.out.println("Using ${schemaContents}")
+        return jsonSchemaValidationService.validateBasic(schemaContents, toValidate)
+        //return validate(toValidate)
+    }
+
+    private fun validate(toValidate: String) : BasicOutput {
+        val output = jsonSchemaValidationService.validateBasic(toValidate)
+        System.out.println("Validation input: ${toValidate}")
         System.out.println("Validation output: ${output}")
         return output
     }
 }
+
+/*fun main(args: Array<String>) {
+    if (args.size != 1) {
+        println("You need to supply path to Page tab file as only argument!")
+        return
+    }
+
+    val pageTabPath = args[0]
+    println("Processing ${pageTabPath}")
+
+
+    val contents = File(pageTabPath).readText()
+    val submission = serializationService.deserializeSubmission(contents, SubFormat.TSV)
+    val submissionJson = serializationService.serializeSubmission(submission, SubFormat.JSON_PRETTY)
+    println("${submissionJson}")
+
+    // Convert into an object
+}
+*/
--- submission/submission-webapp/src/test/kotlin/ac/uk/ebi/biostd/submission/web/resources/JsonSchemaValidationResourceTest.kt
+++ submission/submission-webapp/src/test/kotlin/ac/uk/ebi/biostd/submission/web/resources/JsonSchemaValidationResourceTest.kt
@@ -1,12 +1,17 @@
 package ac.uk.ebi.biostd.submission.web.resources
 
 import ac.uk.ebi.biostd.submission.domain.service.JsonSchemaValidationService
+import ebi.ac.uk.model.constants.APPLICATION_JSON
 import ebi.ac.uk.model.constants.FILE_LIST_NAME
+import ebi.ac.uk.model.constants.SUBMISSION_TYPE
+import ebi.ac.uk.model.constants.TEXT_PLAIN
 import io.mockk.impl.annotations.MockK
 import io.mockk.junit5.MockKExtension
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.mock.web.MockMultipartFile
 import org.springframework.test.web.servlet.get
+import org.springframework.test.web.servlet.multipart
 import org.springframework.test.web.servlet.post
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
@@ -24,11 +29,39 @@ public class JsonSchemaValidationResourceTest(
         .build()
 
     @Test
-    fun `validate post`() {
+    fun `validate json`() {
         mvc.post("/jsonschema-validation") {
+            header("${SUBMISSION_TYPE}", "${APPLICATION_JSON}")
             content = """{ "toValidate": "test.json" }"""
         }.andExpect {
             status { isOk }
         }.andExpect { jsonPath("\$.valid").value(true) }
+    }
+
+    @Test
+    fun `validate tsv`() {
+        mvc.post("/jsonschema-validation") {
+            header("${SUBMISSION_TYPE}", "${TEXT_PLAIN}")
+            content = """Submission
+                Title   Test submission
+                ReleaseDate 2021-11-01
+            """.trimIndent()
+        }.andExpect {
+            status { isOk }
+        }.andExpect { jsonPath("\$.valid").value(true) }
+    }
+
+    @Test
+    fun `fails to validate json with custom schema`() {
+        val customSchema = MockMultipartFile(
+            "schema", "custom-schema.json", "application/json",
+            """{ "test": "schema"}""".toByteArray(charset("UTF8")))
+        mvc.multipart( "/jsonschema-validation/custom-schema") {
+            header("${SUBMISSION_TYPE}", "${ APPLICATION_JSON}")
+            file(customSchema)
+            content = """{ "SchemaValidationFilePath": "custom-schema.json" }"""
+        }.andExpect {
+            status { isOk }
+        }.andExpect { jsonPath("\$.valid").value(false) }
     }
 }
