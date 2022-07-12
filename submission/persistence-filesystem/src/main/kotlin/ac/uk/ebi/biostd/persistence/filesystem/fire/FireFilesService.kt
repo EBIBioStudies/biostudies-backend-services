c15729add8526506ab447f942e8c85c40ce6c278
Juan Camilo Rada Mesa <jcamilorada@ebi.ac.uk>
2022-07-12 08:28:40 -0500

    code review changes

--- submission/persistence-filesystem/src/main/kotlin/ac/uk/ebi/biostd/persistence/filesystem/fire/FireFilesService.kt
+++ submission/persistence-filesystem/src/main/kotlin/ac/uk/ebi/biostd/persistence/filesystem/fire/FireFilesService.kt
@@ -2,7 +2,9 @@ package ac.uk.ebi.biostd.persistence.filesystem.fire
 
 import ac.uk.ebi.biostd.persistence.filesystem.api.FilesService
 import ac.uk.ebi.biostd.persistence.filesystem.request.FilePersistenceRequest
+import ebi.ac.uk.extended.model.ExtFile
 import ebi.ac.uk.extended.model.ExtSubmission
+import ebi.ac.uk.extended.model.FireFile
 import mu.KotlinLogging
 import uk.ac.ebi.extended.serialization.service.FileProcessingService
 
@@ -18,17 +20,29 @@ class FireFilesService(
         logger.info { "${sub.accNo} ${sub.owner} Cleaning Ftp Folder for ${sub.accNo}" }
         fireService.cleanFtp(sub)
         logger.info { "${sub.accNo} ${sub.owner} Finished Ftp Folder for ${sub.accNo}" }
-
-        logger.info { "${sub.accNo} ${sub.owner} Persisting files of submission ${sub.accNo} on FIRE" }
         val processed = processFiles(request.submission)
-        logger.info { "${sub.accNo} ${sub.owner} Finished persisting files of submission ${sub.accNo} on FIRE" }
-
         return processed
     }
 
-    private fun processFiles(sub: ExtSubmission): ExtSubmission =
-        fileProcessingService.processFiles(sub) { file, index ->
+    private fun processFiles(sub: ExtSubmission): ExtSubmission {
+        var newFilesSize = 0L
+        var newFiles = 0
+
+        fun processFile(file: ExtFile, index: Int): FireFile {
             logger.debug { "${sub.accNo}, ${sub.version} Processing file $index, path='${file.filePath}'" }
-            fireService.getOrPersist(sub, file)
+            val (fireFile, created) = fireService.getOrPersist(sub, file)
+            if (created) {
+                newFiles += 1
+                newFilesSize += fireFile.size
+            }
+            logger.debug { "${sub.accNo}, ${sub.version} Finished processing file $index, path='${file.filePath}'" }
+            return fireFile
         }
+
+        logger.info { "${sub.accNo} ${sub.owner} Starting persisting files of submission ${sub.accNo} on FIRE" }
+        val submission = fileProcessingService.processFiles(sub) { file, index -> processFile(file, index) }
+        logger.info { "${sub.accNo} ${sub.owner} Processed $newFiles new files, $newFilesSize bytes on FIRE" }
+        logger.info { "${sub.accNo} ${sub.owner} Finished persisting files of submission ${sub.accNo} on FIRE" }
+        return submission
+    }
 }
