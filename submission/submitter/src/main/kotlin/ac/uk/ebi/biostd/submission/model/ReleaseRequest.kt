791223a9397d79d682e75340375533699bc50f74
Juan Camilo Rada Mesa <jcamilorada@ebi.ac.uk>
2022-07-29 05:47:10 -0500

    added release

--- submission/submitter/src/main/kotlin/ac/uk/ebi/biostd/submission/model/ReleaseRequest.kt
+++ submission/submitter/src/main/kotlin/ac/uk/ebi/biostd/submission/model/ReleaseRequest.kt
@@ -1,7 +1,3 @@
 package ac.uk.ebi.biostd.submission.model
 
-data class ReleaseRequest(
-    val accNo: String,
-    val owner: String,
-    val relPath: String
-)
+data class ReleaseRequest(val accNo: String)
