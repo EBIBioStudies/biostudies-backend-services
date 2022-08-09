360e0a821bed0d86a5f9f3cd83d07a19eb5a55d6
Jhoan Manuel Muñoz Serrano <jhoanmanuelms@gmail.com>
2022-08-09 16:09:13 +0100

    Pivotal ID #182583027: PMC Export Empty Files (#599)
    * Pivotal ID #182583027: PMC Export Empty Files
    
    - Reconnect and retry after failure
    - Remove parallelism
    - Extract FTP logic into a standalone client

--- scheduler/tasks/exporter-task/src/main/kotlin/uk/ac/ebi/scheduler/pmc/exporter/service/PmcExporterService.kt
+++ scheduler/tasks/exporter-task/src/main/kotlin/uk/ac/ebi/scheduler/pmc/exporter/service/PmcExporterService.kt
@@ -1,12 +1,8 @@
 package uk.ac.ebi.scheduler.pmc.exporter.service
 
 import com.fasterxml.jackson.dataformat.xml.XmlMapper
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.joinAll
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 import mu.KotlinLogging
-import org.apache.commons.net.ftp.FTPClient
+import uk.ac.ebi.scheduler.pmc.exporter.cli.BioStudiesFtpClient
 import uk.ac.ebi.scheduler.pmc.exporter.config.ApplicationProperties
 import uk.ac.ebi.scheduler.pmc.exporter.mapping.toLink
 import uk.ac.ebi.scheduler.pmc.exporter.model.Link
@@ -22,45 +18,39 @@ private val logger = KotlinLogging.logger {}
 class PmcExporterService(
     pmcRepository: PmcRepository,
     private val xmlWriter: XmlMapper,
-    private val ftpClient: FTPClient,
+    private val ftpClient: BioStudiesFtpClient,
     private val appProperties: ApplicationProperties,
 ) {
     private val pmcDataIterator = pageIterator(pmcRepository)
 
-    suspend fun exportPmcLinks() = withContext(Dispatchers.IO) {
+    fun exportPmcLinks() {
         logger.info { "Started exporting PMC links" }
 
         ftpClient.login()
 
         pmcDataIterator
             .asSequence()
-            .mapIndexed { part, page -> launch { writeLinks(part + 1, page.content) } }
+            .mapIndexed { part, page -> writeLinks(part + 1, page.content) }
             .toList()
-            .joinAll()
 
         ftpClient.logout()
-        ftpClient.disconnect()
 
         logger.info { "Finished exporting PMC links" }
     }
 
-    private suspend fun writeLinks(part: Int, submissions: List<PmcData>) {
+    private fun writeLinks(part: Int, submissions: List<PmcData>) {
         logger.info { "Exporting PMC links part $part" }
         val links = submissions.map { it.toLink() }
-        withContext(Dispatchers.IO) { write(part, links) }
+
+        write(part, links)
     }
 
     private fun write(part: Int, links: List<Link>) {
-        logger.info { "writing file part $part" }
-        val xml = xmlWriter.writeValueAsString(Links(links))
+        logger.info { "Writing file part $part" }
+
+        val xml = xmlWriter.writeValueAsString(Links(links)).byteInputStream()
         val path = "${appProperties.outputPath}/${String.format(appProperties.fileName, part)}"
-        ftpClient.storeFile(path, xml.byteInputStream())
-    }
 
-    private fun FTPClient.login() {
-        val ftpConfig = appProperties.ftp
-
-        connect(ftpConfig.host, ftpConfig.port.toInt())
-        login(ftpConfig.user, ftpConfig.password)
+        ftpClient.storeFile(path, xml)
     }
 }
