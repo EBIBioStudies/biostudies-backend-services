8c312ac3694452a5adfe4494bf0de5567fab349e
Juan Camilo Rada Mesa <jcamilorada@ebi.ac.uk>
2022-08-22 08:31:27 -0500

    added logging

--- scheduler/common/cluster-client/src/main/kotlin/ac/uk/ebi/cluster/client/lsf/ClusterOperations.kt
+++ scheduler/common/cluster-client/src/main/kotlin/ac/uk/ebi/cluster/client/lsf/ClusterOperations.kt
@@ -7,10 +7,12 @@ import ac.uk.ebi.cluster.client.model.JobSpec
 import arrow.core.Try
 import com.jcraft.jsch.JSch
 import com.jcraft.jsch.Session
+import mu.KotlinLogging
 
 private const val AS_SYSTEM_USER = "sudo -u $SYSTEM_USER $LSF_ENVDIR $LSF_SERVERDIR"
 private const val REDIRECT_LOGS = "-o $LOGS_PATH%J_OUT -e $LOGS_PATH/%J_IN"
 private const val SUBMIT_COMMAND = "$AS_SYSTEM_USER bsub $REDIRECT_LOGS"
+private val logger = KotlinLogging.logger {}
 
 class ClusterOperations(
     private val responseParser: JobResponseParser,
@@ -28,11 +30,12 @@ class ClusterOperations(
         }
     }
 
-    private fun asJobReturn(exitCode: Int, response: String) =
-        if (exitCode == 0)
-            Try.just(responseParser.toJob(response))
-        else
-            Try.raise(JobSubmitFailException(response))
+    private fun asJobReturn(exitCode: Int, response: String): Try<Job> {
+        if (exitCode == 0) return Try.just(responseParser.toJob(response))
+
+        logger.error(response) { "Error submission job, exitCode='$exitCode', response='$response'" }
+        return Try.raise(JobSubmitFailException(response))
+    }
 
     companion object {
         private val responseParser = JobResponseParser()
