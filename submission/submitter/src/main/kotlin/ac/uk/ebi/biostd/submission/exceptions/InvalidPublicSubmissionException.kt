09df5e4e4c30291285b3e20897e15ce6f14ba167
Jhoan Munoz <jhoanmanuelms@gmail.com>
2023-06-08 14:57:15 +0100

    Pivotal ID # 182595703: Submission Suppressing Workflow
    Improve testing

--- submission/submitter/src/main/kotlin/ac/uk/ebi/biostd/submission/exceptions/InvalidPublicSubmissionException.kt
+++ submission/submitter/src/main/kotlin/ac/uk/ebi/biostd/submission/exceptions/InvalidPublicSubmissionException.kt
@@ -1,9 +1,5 @@
 package ac.uk.ebi.biostd.submission.exceptions
 
-class InvalidDateFormatException(date: String) : RuntimeException(
-    "Provided date $date could not be parsed. Expected format is YYYY-MM-DD"
-)
-
 class PastReleaseDateException : RuntimeException("Release date cannot be in the past")
 
 class InvalidReleaseDateException : RuntimeException("The release date of a public study cannot be changed")
