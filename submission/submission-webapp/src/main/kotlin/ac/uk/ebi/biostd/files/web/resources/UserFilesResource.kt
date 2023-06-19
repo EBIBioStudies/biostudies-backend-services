4cf7369905f98ff3fc6d99b660ddc39d12413703
Juan <jcamilorada@ebi.ac.uk>
2023-06-18 22:17:04 -0500

    Pivotal ID #184977333: Investigate changes to Files API - use FTP (#723)
    https://www.pivotaltracker.com/story/show/184977333
    - Created single file api

--- submission/submission-webapp/src/main/kotlin/ac/uk/ebi/biostd/files/web/resources/UserFilesResource.kt
+++ submission/submission-webapp/src/main/kotlin/ac/uk/ebi/biostd/files/web/resources/UserFilesResource.kt
@@ -1,6 +1,6 @@
 package ac.uk.ebi.biostd.files.web.resources
 
-import ac.uk.ebi.biostd.files.service.UserFilesService
+import ac.uk.ebi.biostd.files.service.PathFilesService
 import ac.uk.ebi.biostd.files.web.common.FilesMapper
 import ac.uk.ebi.biostd.files.web.common.UserPath
 import ac.uk.ebi.biostd.submission.converters.BioUser
@@ -26,15 +26,15 @@ import org.springframework.web.multipart.MultipartFile
 @PreAuthorize("isAuthenticated()")
 class UserFilesResource(
     private val filesMapper: FilesMapper,
-    private val fileManager: UserFilesService
 ) {
     @GetMapping("/files/user/**")
     @ResponseBody
     fun listFiles(
         @BioUser user: SecurityUser,
-        pathDescriptor: UserPath
+        pathDescriptor: UserPath,
     ): List<UserFile> {
-        return filesMapper.asUserFiles(fileManager.listFiles(user, pathDescriptor.path))
+        val filesService = PathFilesService.forUser(user)
+        return filesMapper.asUserFiles(filesService.listFiles(pathDescriptor.path))
     }
 
     @GetMapping("/files/user/**", produces = [MediaType.APPLICATION_OCTET_STREAM_VALUE], params = ["fileName"])
@@ -42,17 +42,15 @@ class UserFilesResource(
     fun downloadFile(
         @BioUser user: SecurityUser,
         @RequestParam(name = "fileName") fileName: String,
-        pathDescriptor: UserPath
+        pathDescriptor: UserPath,
     ): ResponseEntity<FileSystemResource> {
-        val fileSystemResource = FileSystemResource(
-            fileManager.getFile(user, pathDescriptor.path, fileName)
-        )
+        val filesService = PathFilesService.forUser(user)
+        val fileSystemResource = FileSystemResource(filesService.getFile(pathDescriptor.path, fileName))
         val contentDisposition = ContentDisposition
             .builder("inline")
             .filename(fileSystemResource.filename)
             .build()
         val headers = HttpHeaders().apply { setContentDisposition(contentDisposition) }
-
         return ResponseEntity
             .ok()
             .headers(headers)
@@ -65,22 +63,31 @@ class UserFilesResource(
     fun uploadFile(
         @BioUser user: SecurityUser,
         pathDescriptor: UserPath,
-        @RequestParam("files") files: Array<MultipartFile>
-    ) = fileManager.uploadFiles(user, pathDescriptor.path, files.toList())
+        @RequestParam("files") files: Array<MultipartFile>,
+    ) {
+        val filesService = PathFilesService.forUser(user)
+        filesService.uploadFiles(pathDescriptor.path, files.toList())
+    }
 
     @DeleteMapping("/files/user/**")
     @ResponseStatus(value = HttpStatus.OK)
     fun deleteFile(
         @BioUser user: SecurityUser,
         @RequestParam(name = "fileName") fileName: String,
-        pathDescriptor: UserPath
-    ) = fileManager.deleteFile(user, pathDescriptor.path, fileName)
+        pathDescriptor: UserPath,
+    ) {
+        val filesService = PathFilesService.forUser(user)
+        filesService.deleteFile(pathDescriptor.path, fileName)
+    }
 
     @PostMapping("/folder/user/**")
     @ResponseStatus(value = HttpStatus.OK)
     fun createFolder(
         @BioUser user: SecurityUser,
         @RequestParam(name = "folder") folder: String,
-        pathDescriptor: UserPath
-    ) = fileManager.createFolder(user, pathDescriptor.path, folder)
+        pathDescriptor: UserPath,
+    ) {
+        val filesService = PathFilesService.forUser(user)
+        filesService.createFolder(pathDescriptor.path, folder)
+    }
 }
