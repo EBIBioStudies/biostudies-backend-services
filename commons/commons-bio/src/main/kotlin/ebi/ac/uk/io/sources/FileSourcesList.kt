8558e2468241b91120d67b9a765cea90be0f7aa8
Jhoan Munoz <jhoanmanuelms@gmail.com>
2023-07-03 14:52:50 +0100

    Pivotal ID # 185352396: Fixed Concurrency File Operations
    Improve documentation

--- commons/commons-bio/src/main/kotlin/ebi/ac/uk/io/sources/FileSourcesList.kt
+++ commons/commons-bio/src/main/kotlin/ebi/ac/uk/io/sources/FileSourcesList.kt
@@ -6,6 +6,22 @@ import ebi.ac.uk.extended.model.ExtFile
 import ebi.ac.uk.model.Attribute
 import java.io.File
 
+/**
+ * Regex pattern used to match the suggested charset for S3 keys described at:
+ * https://docs.aws.amazon.com/AmazonS3/latest/userguide/object-keys.html
+ *
+ * - Avoid relative paths (./ or ../)
+ * - Allow any alphanumeric character (a-z | A-Z | 0-9)
+ * - Allow any of the following special characters:
+ *     - Exclamation point (!)
+ *     - Hyphen (-)
+ *     - Underscore (_)
+ *     - Period (.)
+ *     - Asterisk (*)
+ *     - Single quote (')
+ *     - Open parenthesis ( ( )
+ *     - Close parenthesis ( ) )
+ */
 private val validPathPattern = "^(?!.*\\./)[0-9A-Za-z!-_*'(). ]+\$".toRegex()
 
 @JvmInline
