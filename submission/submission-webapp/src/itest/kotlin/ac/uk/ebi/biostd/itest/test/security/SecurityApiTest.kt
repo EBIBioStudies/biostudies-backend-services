2d000af829d9edd4886e6614a3b26d0164a246cf
Jhoan Munoz <jhoanmanuelms@gmail.com>
2023-07-04 17:53:59 +0100

    Pivotal ID # 185110100: Case Insensitive Submission Ownership
    Change the email to lowercase at registration

--- submission/submission-webapp/src/itest/kotlin/ac/uk/ebi/biostd/itest/test/security/SecurityApiTest.kt
+++ submission/submission-webapp/src/itest/kotlin/ac/uk/ebi/biostd/itest/test/security/SecurityApiTest.kt
@@ -3,21 +3,26 @@ package ac.uk.ebi.biostd.itest.test.security
 import ac.uk.ebi.biostd.client.exception.WebClientException
 import ac.uk.ebi.biostd.client.integration.web.SecurityWebClient
 import ac.uk.ebi.biostd.itest.entities.TestUser
+import ac.uk.ebi.biostd.persistence.repositories.UserDataRepository
 import ebi.ac.uk.api.security.CheckUserRequest
 import ebi.ac.uk.api.security.LoginRequest
 import ebi.ac.uk.api.security.RegisterRequest
+import org.assertj.core.api.Assertions.assertThat
 import org.assertj.core.api.Assertions.assertThatExceptionOfType
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.web.server.LocalServerPort
 import org.springframework.test.context.junit.jupiter.SpringExtension
+import kotlin.test.assertNotNull
 
 @ExtendWith(SpringExtension::class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class SecurityApiTest(
     @LocalServerPort val serverPort: Int,
+    @Autowired private val userDataRepository: UserDataRepository,
 ) {
     private lateinit var webClient: SecurityWebClient
 
@@ -47,6 +52,26 @@ class SecurityApiTest(
         assertThatExceptionOfType(WebClientException::class.java)
             .isThrownBy { webClient.login(LoginRequest(InactiveUser.email, InactiveUser.password)) }
             .withMessageContaining("Authentication Error: Invalid email address or password")
+    }
+
+    @Test
+    fun `22-4 case insensitive user registration`() {
+        val request = RegisterRequest("User", "Case-Insensitive@mail.org", "123")
+        webClient.registerUser(request)
+
+        val user = userDataRepository.findByEmailAndActive("Case-Insensitive@mail.org", true)
+        assertNotNull(user)
+        assertThat(user.email).isEqualTo("case-insensitive@mail.org")
+    }
+
+    @Test
+    fun `22-5 case insensitive inactive registration`() {
+        val request = CheckUserRequest("Case-Insensitive-Inactive@mail.org", "User")
+        webClient.checkUser(request)
+
+        val user = userDataRepository.findByEmailAndActive("Case-Insensitive-Inactive@mail.org", false)
+        assertNotNull(user)
+        assertThat(user.email).isEqualTo("case-insensitive-inactive@mail.org")
     }
 
     object NewUser : TestUser {
