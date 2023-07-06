06f6ae035146faf9f339a70c1d5f003e6e3b471c
Juan Camilo Rada Mesa <jcamilorada@ebi.ac.uk>
2023-07-05 21:19:38 -0500

    3

--- submission/submission-webapp/src/itest/kotlin/ac/uk/ebi/biostd/itest/itest/FtpContainer.kt
+++ submission/submission-webapp/src/itest/kotlin/ac/uk/ebi/biostd/itest/itest/FtpContainer.kt
@@ -1,17 +1,21 @@
 package ac.uk.ebi.biostd.itest.itest
 
 import org.testcontainers.containers.FixedHostPortGenericContainer
+import org.testcontainers.containers.wait.strategy.HostPortWaitStrategy
+import java.net.ServerSocket
 
 data class CreationParams(val user: String, val password: String, val version: String)
 
 class FtpContainer(private val params: CreationParams) :
     FixedHostPortGenericContainer<FtpContainer>("delfer/alpine-ftp-server:${params.version}") {
     override fun start() {
-        withFixedExposedPort(21000, 21000)
-            .withEnv("MIN_PORT", "21000")
-            .withEnv("MAX_PORT", "21000")
+        val freePort = ServerSocket(0).use { socket -> socket.localPort }
+        withFixedExposedPort(freePort, freePort)
+            .withEnv("MIN_PORT", freePort.toString())
+            .withEnv("MAX_PORT", freePort.toString())
             .withExposedPorts(21)
             .withEnv("USERS", "${params.user}|${params.password}")
+            .waitingFor(FtpPortWaitStrategy)
         super.start()
     }
 
@@ -21,5 +25,11 @@ class FtpContainer(private val params: CreationParams) :
 
     fun getFtpPort(): Int {
         return getMappedPort(21)
+    }
+}
+
+object FtpPortWaitStrategy : HostPortWaitStrategy() {
+    override fun getLivenessCheckPorts(): Set<Int> {
+        return setOf(waitStrategyTarget.getMappedPort(21))
     }
 }
