58235a3c4ea32b3d1594ac163441d316e810eeb7
Juan Camilo Rada Mesa <jcamilorada@ebi.ac.uk>
2023-07-05 20:24:11 -0500

    2

--- submission/submission-webapp/src/itest/kotlin/ac/uk/ebi/biostd/itest/itest/FtpContainer.kt
+++ submission/submission-webapp/src/itest/kotlin/ac/uk/ebi/biostd/itest/itest/FtpContainer.kt
@@ -1,17 +1,15 @@
 package ac.uk.ebi.biostd.itest.itest
 
 import org.testcontainers.containers.FixedHostPortGenericContainer
-import java.net.ServerSocket
 
 data class CreationParams(val user: String, val password: String, val version: String)
 
 class FtpContainer(private val params: CreationParams) :
     FixedHostPortGenericContainer<FtpContainer>("delfer/alpine-ftp-server:${params.version}") {
     override fun start() {
-        val freePort = ServerSocket(0).use { socket -> socket.localPort }
-        withFixedExposedPort(freePort, freePort)
-            .withEnv("MIN_PORT", freePort.toString())
-            .withEnv("MAX_PORT", freePort.toString())
+        withFixedExposedPort(21000, 21000)
+            .withEnv("MIN_PORT", "21000")
+            .withEnv("MAX_PORT", "21000")
             .withExposedPorts(21)
             .withEnv("USERS", "${params.user}|${params.password}")
         super.start()
