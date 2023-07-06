dae36ce59b009c31cfc2bf9a0e6746d2b10b1f01
Juan Camilo Rada Mesa <jcamilorada@ebi.ac.uk>
2023-07-06 09:20:15 -0500

    code review changes

--- submission/submission-webapp/src/itest/kotlin/ac/uk/ebi/biostd/itest/itest/FtpContainer.kt
+++ submission/submission-webapp/src/itest/kotlin/ac/uk/ebi/biostd/itest/itest/FtpContainer.kt
@@ -2,23 +2,27 @@ package ac.uk.ebi.biostd.itest.itest
 
 import org.testcontainers.containers.FixedHostPortGenericContainer
 import org.testcontainers.containers.wait.strategy.HostPortWaitStrategy
+import java.net.ServerSocket
+import java.net.Socket
 import java.time.Duration
+
 
 data class CreationParams(val user: String, val password: String, val version: String)
 
 class FtpContainer(private val params: CreationParams) :
     FixedHostPortGenericContainer<FtpContainer>("stilliard/pure-ftpd") {
     override fun start() {
-        withFixedExposedPort(30000, 30000)
-        withFixedExposedPort(30001, 30001)
-        withFixedExposedPort(30002, 30002)
-        withFixedExposedPort(30003, 30003)
+        val freePort = findPortRange()
+        withFixedExposedPort(freePort, freePort)
+        withFixedExposedPort(freePort + 1, freePort + 1)
+        withFixedExposedPort(freePort + 2, freePort + 2)
+        withFixedExposedPort(freePort + 3, freePort + 3)
         withExposedPorts(21)
             .withEnv("FTP_USER_NAME", params.user)
             .withEnv("FTP_USER_PASS", params.password)
             .withEnv("ADDED_FLAGS", "--tls=1")
             .withEnv("TLS_USE_DSAPRAM", "true")
-            .withEnv("FTP_PASSIVE_PORTS", "30000:30003")
+            .withEnv("FTP_PASSIVE_PORTS", "$freePort:${freePort + 3}")
             .withEnv("PUBLICHOST", "0.0.0.0")
             .withEnv("FTP_USER_HOME", "/home/ftpUser")
             .withEnv("TLS_CN", "localhost")
@@ -36,6 +40,17 @@ class FtpContainer(private val params: CreationParams) :
         return getMappedPort(21)
     }
 }
+
+fun findPortRange(): Int {
+    val one = ServerSocket(0).use { socket -> socket.localPort }
+    val two = one + 1
+    val three = two + 1
+    val four = three + 1
+    return if (availablePort(two) && availablePort(three) && availablePort(four)) one else findPortRange()
+}
+
+private fun availablePort(port: Int): Boolean =
+    runCatching { Socket("localhost", port).close(); return false }.getOrElse { true }
 
 object FtpPortWaitStrategy : HostPortWaitStrategy() {
     init {
