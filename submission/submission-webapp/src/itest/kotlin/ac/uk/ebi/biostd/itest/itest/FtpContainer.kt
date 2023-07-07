64a61ad2ed0ce51362366c8e84ad0cc295713ce3
Juan Camilo Rada Mesa <jcamilorada@ebi.ac.uk>
2023-07-06 23:32:26 -0500

    code review changes

--- submission/submission-webapp/src/itest/kotlin/ac/uk/ebi/biostd/itest/itest/FtpContainer.kt
+++ submission/submission-webapp/src/itest/kotlin/ac/uk/ebi/biostd/itest/itest/FtpContainer.kt
@@ -1,61 +1,61 @@
 package ac.uk.ebi.biostd.itest.itest
 
 import org.testcontainers.containers.FixedHostPortGenericContainer
-import org.testcontainers.containers.wait.strategy.HostPortWaitStrategy
 import java.net.ServerSocket
 import java.net.Socket
-import java.time.Duration
 
 data class CreationParams(val user: String, val password: String, val version: String)
 
 class FtpContainer(private val params: CreationParams) :
     FixedHostPortGenericContainer<FtpContainer>("stilliard/pure-ftpd") {
     override fun start() {
-        val freePort = findPortRange()
-        withFixedExposedPort(freePort, freePort)
-        withFixedExposedPort(freePort + 1, freePort + 1)
-        withFixedExposedPort(freePort + 2, freePort + 2)
-        withFixedExposedPort(freePort + 3, freePort + 3)
-        withExposedPorts(21)
+        val (p1, p2, p3, p4) = findPortRange()
+        withFixedExposedPort(p1, p1)
+        withFixedExposedPort(p2, p2)
+        withFixedExposedPort(p3, p3)
+        withFixedExposedPort(p4, p4)
+        withExposedPorts(FTP_PORT)
             .withEnv("FTP_USER_NAME", params.user)
             .withEnv("FTP_USER_PASS", params.password)
             .withEnv("ADDED_FLAGS", "--tls=1")
             .withEnv("TLS_USE_DSAPRAM", "true")
-            .withEnv("FTP_PASSIVE_PORTS", "$freePort:${freePort + 3}")
-            .withEnv("PUBLICHOST", "ftp")
+            .withEnv("FTP_PASSIVE_PORTS", "$p1:$p4")
+            .withEnv("PUBLICHOST", containerIpAddress)
             .withEnv("FTP_USER_HOME", "/home/ftpUser")
             .withEnv("TLS_CN", "localhost")
             .withEnv("TLS_ORG", "YourOrg")
             .withEnv("TLS_C", "DE")
-            .waitingFor(FtpPortWaitStrategy)
-            .withCreateContainerCmdModifier { cmd -> cmd.withName("ftp") }
         super.start()
     }
 
     fun getUrl(): String {
-        return "ftp"
+        return containerIpAddress
     }
 
     fun getFtpPort(): Int {
-        return getMappedPort(21)
-    }
-}
-
-fun findPortRange(): Int {
-    val one = ServerSocket(0).use { socket -> socket.localPort }
-    val two = one + 1
-    val three = two + 1
-    val four = three + 1
-    return if (availablePort(two) && availablePort(three) && availablePort(four)) one else findPortRange()
-}
-
-private fun availablePort(port: Int): Boolean =
-    runCatching { Socket("localhost", port).close(); return false }.getOrElse { true }
-
-object FtpPortWaitStrategy : HostPortWaitStrategy() {
-    init {
-        startupTimeout = Duration.ofMinutes(10)
+        return getMappedPort(FTP_PORT)
     }
 
-    override fun getLivenessCheckPorts(): Set<Int> = setOf(waitStrategyTarget.getMappedPort(21))
+    private companion object {
+        const val FTP_PORT = 21
+
+        fun findPortRange(): PortRange {
+            val one = ServerSocket(0).use { socket -> socket.localPort }
+            val two = one + 1
+            val three = two + 1
+            val four = three + 1
+            return when {
+                availablePort(two) && availablePort(three) && availablePort(four) -> PortRange(one, two, three, four)
+                else -> findPortRange()
+            }
+        }
+
+        private fun availablePort(port: Int): Boolean =
+            runCatching {
+                Socket("localhost", port).close()
+                return false
+            }.getOrElse { true }
+    }
+
+    private data class PortRange(val port1: Int, val port2: Int, val port3: Int, val port4: Int)
 }
