578296f021d6089da45b4574c8638ec9594b8e19
Juan Camilo Rada Mesa <jcamilorada@ebi.ac.uk>
2023-08-24 08:32:00 -0500

    added test back

--- submission/submission-webapp/src/itest/kotlin/ac/uk/ebi/biostd/itest/test/submission/submit/SubmissionApiTest.kt
+++ submission/submission-webapp/src/itest/kotlin/ac/uk/ebi/biostd/itest/test/submission/submit/SubmissionApiTest.kt
@@ -33,7 +33,7 @@ import org.assertj.core.api.Assertions.assertThat
 import org.assertj.core.api.Assertions.assertThatExceptionOfType
 import org.junit.jupiter.api.Assertions.assertThrows
 import org.junit.jupiter.api.BeforeAll
-import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.condition.EnabledIfSystemProperty
@@ -334,8 +334,8 @@ class SubmissionApiTest(
     }
 
     @Test
-    @DisplayName("16-12 User with Ftp based folder submission")
-    fun userWithFTpBasedFolder() {
+    @Disabled
+    fun `16-12 User with Ftp based folder submission`() {
         securityTestService.ensureUserRegistration(FtpSuperUser)
         webClient = getWebClient(serverPort, FtpSuperUser)
 
