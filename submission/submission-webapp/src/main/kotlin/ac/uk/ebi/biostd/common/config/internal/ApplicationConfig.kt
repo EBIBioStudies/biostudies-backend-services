bd23004ab78d33fb97614df8e9ed5348b130d468
Juan <jcamilorada@ebi.ac.uk>
2023-11-06 05:35:27 -0800

    Pivotal ID # 186399181: Retry system properties (#766)
    https://www.pivotaltracker.com/story/show/186399181
    - added retry property

--- submission/submission-webapp/src/main/kotlin/ac/uk/ebi/biostd/common/config/internal/ApplicationConfig.kt
+++ submission/submission-webapp/src/main/kotlin/ac/uk/ebi/biostd/common/config/internal/ApplicationConfig.kt
@@ -3,6 +3,7 @@ package ac.uk.ebi.biostd.common.config.internal
 import ac.uk.ebi.biostd.persistence.common.service.SubmissionRequestPersistenceService
 import ac.uk.ebi.biostd.submission.domain.extended.ExtSubmissionService
 import ac.uk.ebi.biostd.submission.domain.service.RetryHandler
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
@@ -10,6 +11,7 @@ import org.springframework.context.annotation.Configuration
 class ApplicationConfig {
 
     @Bean
+    @ConditionalOnProperty(prefix = "app", name = ["retrySubmission"], havingValue = "true")
     fun startApplicationHandler(
         extSubmissionService: ExtSubmissionService,
         requestService: SubmissionRequestPersistenceService,
