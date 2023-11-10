5b64bc921be8492cf5f5ee98f2cd8498032ab954
Jhoan Munoz <jhoanmanuelms@gmail.com>
2023-11-10 11:05:40 +0000

    Pivotal ID # 185649200: Cater For Paths Ending With Backslash
    - Include the file list help url in the error message for invalid paths
    - Include the task to auto deploy the stats reporter task which was forgotten in a previous PR

--- commons/commons-bio/src/test/kotlin/ebi/ac/uk/io/sources/FileSourcesListTest.kt
+++ commons/commons-bio/src/test/kotlin/ebi/ac/uk/io/sources/FileSourcesListTest.kt
@@ -62,7 +62,12 @@ internal class FileSourcesListTest(
             val error = assertFailsWith<InvalidPathException> {
                 testInstance.findExtFile("./folder/file.txt", FILE_TYPE.value, attributes)
             }
-            assertThat(error.message).isEqualTo("The given file path contains invalid characters: ./folder/file.txt")
+            val expectedErrorMessage =
+                """
+                    The given file path contains invalid characters: ./folder/file.txt
+                    For more information check https://www.ebi.ac.uk/bioimage-archive/help-file-list
+                """.trimIndent()
+            assertThat(error.message).isEqualToIgnoringWhitespace(expectedErrorMessage)
         }
 
         @Test
@@ -70,7 +75,12 @@ internal class FileSourcesListTest(
             val error = assertFailsWith<InvalidPathException> {
                 testInstance.findExtFile("folder/../file.txt", FILE_TYPE.value, attributes)
             }
-            assertThat(error.message).isEqualTo("The given file path contains invalid characters: folder/../file.txt")
+            val expectedErrorMessage =
+                """
+                    The given file path contains invalid characters: folder/../file.txt
+                    For more information check https://www.ebi.ac.uk/bioimage-archive/help-file-list
+                """.trimIndent()
+            assertThat(error.message).isEqualToIgnoringWhitespace(expectedErrorMessage)
         }
 
         @Test
@@ -78,7 +88,12 @@ internal class FileSourcesListTest(
             val error = assertFailsWith<InvalidPathException> {
                 testInstance.findExtFile("folder/filé.txt", FILE_TYPE.value, attributes)
             }
-            assertThat(error.message).isEqualTo("The given file path contains invalid characters: folder/filé.txt")
+            val expectedErrorMessage =
+                """
+                    The given file path contains invalid characters: folder/filé.txt
+                    For more information check https://www.ebi.ac.uk/bioimage-archive/help-file-list
+                """.trimIndent()
+            assertThat(error.message).isEqualToIgnoringWhitespace(expectedErrorMessage)
         }
 
         @Test
@@ -86,7 +101,12 @@ internal class FileSourcesListTest(
             val error = assertFailsWith<InvalidPathException> {
                 testInstance.findExtFile("folder/inner/", DIRECTORY_TYPE.value, attributes)
             }
-            assertThat(error.message).isEqualTo("The given file path contains invalid characters: folder/inner/")
+            val expectedErrorMessage =
+                """
+                    The given file path contains invalid characters: folder/inner/
+                    For more information check https://www.ebi.ac.uk/bioimage-archive/help-file-list
+                """.trimIndent()
+            assertThat(error.message).isEqualToIgnoringWhitespace(expectedErrorMessage)
         }
     }
 }
