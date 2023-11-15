e57cabe77f26fddf972bb1de83b69a73b6e1d0ed
Jhoan Manuel Muñoz Serrano <jhoanmanuelms@gmail.com>
2023-11-15 14:02:22 +0000

    Pivotal ID # 185649200: Cater For Paths Ending With Backslash (#771)
    https://www.pivotaltracker.com/story/show/185649200
    
    - Include the file list help url in the error message for invalid paths
    - Include the task to auto deploy the stats reporter task which was
    forgotten in a previous PR

--- commons/commons-bio/src/main/kotlin/ebi/ac/uk/errors/InvalidPathException.kt
+++ commons/commons-bio/src/main/kotlin/ebi/ac/uk/errors/InvalidPathException.kt
@@ -1,3 +1,9 @@
 package ebi.ac.uk.errors
 
-class InvalidPathException(path: String) : RuntimeException("The given file path contains invalid characters: $path")
+class InvalidPathException(val path: String) : RuntimeException() {
+    override val message: String
+        get() = """
+            The given file path contains invalid characters: $path
+            For more information check https://www.ebi.ac.uk/bioimage-archive/help-file-list
+        """
+}
