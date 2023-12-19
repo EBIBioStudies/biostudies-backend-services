0a275ed5dda0d4dad02a0b99625be3ce1b5c0ee4
Jhoan Munoz <jhoanmanuelms@gmail.com>
2023-12-19 17:57:12 +0000

    Pivotal ID # 186629008: Slack Message For Failed Notifications
    - Add message converter to the rabbit template
    - Configure custom exception handler to catch failing messages

--- submission/submission-handlers/src/main/kotlin/ac/uk/ebi/biostd/handlers/config/ApplicationConfig.kt
+++ submission/submission-handlers/src/main/kotlin/ac/uk/ebi/biostd/handlers/config/ApplicationConfig.kt
@@ -1,6 +1,7 @@
 package ac.uk.ebi.biostd.handlers.config
 
 import ac.uk.ebi.biostd.handlers.api.BioStudiesWebConsumer
+import ac.uk.ebi.biostd.handlers.exception.CustomErrorHandler
 import ac.uk.ebi.biostd.handlers.listeners.LogSubmissionListener
 import ac.uk.ebi.biostd.handlers.listeners.SecurityNotificationListener
 import ac.uk.ebi.biostd.handlers.listeners.SubmissionNotificationsListener
@@ -10,7 +11,12 @@ import ebi.ac.uk.commons.http.slack.NotificationsSender
 import ebi.ac.uk.notifications.integration.NotificationConfig
 import ebi.ac.uk.notifications.service.RtNotificationService
 import ebi.ac.uk.notifications.service.SecurityNotificationService
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory
+import org.springframework.amqp.rabbit.connection.ConnectionFactory
 import org.springframework.amqp.rabbit.core.RabbitTemplate
+import org.springframework.amqp.rabbit.listener.ConditionalRejectingErrorHandler
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter
+import org.springframework.boot.autoconfigure.amqp.SimpleRabbitListenerContainerFactoryConfigurer
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Import
@@ -24,6 +30,23 @@ import uk.ac.ebi.extended.serialization.service.ExtSerializationService
 class Listeners {
     @Bean
     fun logListener(): LogSubmissionListener = LogSubmissionListener()
+
+    @Bean
+    fun customErrorHandler(
+        notificationsSender: NotificationsSender,
+    ): ConditionalRejectingErrorHandler = CustomErrorHandler(notificationsSender)
+
+    @Bean
+    fun rabbitTemplate(
+        connectionFactory: ConnectionFactory,
+        factory: SimpleRabbitListenerContainerFactory,
+        errorHandler: ConditionalRejectingErrorHandler,
+        configurer: SimpleRabbitListenerContainerFactoryConfigurer,
+    ): RabbitTemplate {
+        factory.setErrorHandler(errorHandler)
+        configurer.configure(factory, connectionFactory)
+        return RabbitTemplate(connectionFactory).apply { messageConverter = Jackson2JsonMessageConverter() }
+    }
 
     @Bean
     fun notificationsListener(
