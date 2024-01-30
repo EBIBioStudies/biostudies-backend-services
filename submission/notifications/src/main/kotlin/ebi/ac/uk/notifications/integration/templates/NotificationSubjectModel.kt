26c8a9975fa5a1121e56911aadd80661fc59711c
Jhoan Manuel Muñoz Serrano <jhoanmanuelms@gmail.com>
2024-01-30 15:32:05 +0000

    Pivotal ID # 186547510: Configurable Notifications E-Mail Subject (#800)
    https://www.pivotaltracker.com/story/show/186547510
    
    - Use the configuration mechanism to set the e-mail subject
    - Use constructor binding for the handlers app configuration in order to
    avoid missing properties
    - Remove unused properties
    - Improve tests

---
+++ submission/notifications/src/main/kotlin/ebi/ac/uk/notifications/integration/templates/NotificationSubjectModel.kt
@@ -0,0 +1,14 @@
+package ebi.ac.uk.notifications.integration.templates
+
+import ebi.ac.uk.notifications.integration.model.NotificationTemplate
+import ebi.ac.uk.notifications.integration.model.NotificationTemplateModel
+
+internal class NotificationSubjectTemplate(content: String) : NotificationTemplate<NotificationSubjectModel>(content)
+
+internal class NotificationSubjectModel(
+    private val accNo: String
+) : NotificationTemplateModel {
+    override fun getParams(): List<Pair<String, String>> = listOf(
+        "ACC_NO" to accNo,
+    )
+}
