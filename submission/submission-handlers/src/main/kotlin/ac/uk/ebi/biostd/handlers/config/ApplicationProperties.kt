1d83d353f70ed952235f347b055b1dc747fb9389
Juan Camilo Rada Mesa <jcamilorada@ebi.ac.uk>
2024-02-12 12:35:00 -0500

    code review changes

--- submission/submission-handlers/src/main/kotlin/ac/uk/ebi/biostd/handlers/config/ApplicationProperties.kt
+++ submission/submission-handlers/src/main/kotlin/ac/uk/ebi/biostd/handlers/config/ApplicationProperties.kt
@@ -2,12 +2,12 @@ package ac.uk.ebi.biostd.handlers.config
 
 import ac.uk.ebi.biostd.common.properties.NotificationProperties
 import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.boot.context.properties.ConstructorBinding
 import org.springframework.boot.context.properties.NestedConfigurationProperty
 
 @ConfigurationProperties(prefix = "app")
+@ConstructorBinding
 data class ApplicationProperties(
-    val baseInstanceUrl: String,
-
     @NestedConfigurationProperty
     val notifications: NotificationProperties,
 )
