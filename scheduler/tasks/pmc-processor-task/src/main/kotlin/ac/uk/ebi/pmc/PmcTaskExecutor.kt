cd6ebbd2cf24a01f685d814ba680f986a96c476c
Juan Camilo Rada Mesa <jcamilorada@ebi.ac.uk>
2024-02-14 22:15:54 -0500

    code review changes

--- scheduler/tasks/pmc-processor-task/src/main/kotlin/ac/uk/ebi/pmc/PmcTaskExecutor.kt
+++ scheduler/tasks/pmc-processor-task/src/main/kotlin/ac/uk/ebi/pmc/PmcTaskExecutor.kt
@@ -4,6 +4,7 @@ import ac.uk.ebi.pmc.load.PmcFileLoader
 import ac.uk.ebi.pmc.process.PmcProcessor
 import ac.uk.ebi.pmc.submit.PmcSubmitter
 import ac.uk.ebi.scheduler.properties.PmcImporterProperties
+import ac.uk.ebi.scheduler.properties.PmcMode
 import ac.uk.ebi.scheduler.properties.PmcMode.LOAD
 import ac.uk.ebi.scheduler.properties.PmcMode.PROCESS
 import ac.uk.ebi.scheduler.properties.PmcMode.SUBMIT
@@ -37,31 +38,42 @@ class PmcTaskExecutor(
      */
     @Suppress("TooGenericExceptionCaught")
     fun run() {
-        val mode = props.mode
-        runCatching {
-            when (mode) {
-                LOAD -> {
-                    val folder = File(requireNotNull(props.loadFolder) { "load folder parameter is required" })
-                    val file = props.loadFile?.let { folder.resolve(it) }
-                    context.getBean<PmcFileLoader>().loadFile(folder, file)
-                }
+        runCatching { runProcess(props.mode) }.fold({ onSuccess() }, { onError(it) })
+    }
 
-                PROCESS -> context.getBean<PmcProcessor>().processAll(props.sourceFile)
-                SUBMIT -> context.getBean<PmcSubmitter>().submitAll(props.sourceFile)
-                SUBMIT_SINGLE -> context.getBean<PmcSubmitter>().submitSingle(requireNotNull(props.submissionId))
-            }
-        }.fold(
-            {
-                runBlocking {
-                    notificationSender.send(Report(SYSTEM, mode.description, "Process was completed successfully"))
-                }
-            },
-            {
-                logger.error(it) { "Error executing pmc task, mode = ${props.mode} " }
-                runBlocking {
-                    notificationSender.send(Alert(SYSTEM, mode.description, "Error executing process", it.message))
-                }
-            },
+    private fun onError(it: Throwable) = runBlocking {
+        logger.error(it) { "Error executing pmc task, mode = ${props.mode} " }
+        notificationSender.send(
+            Alert(
+                SYSTEM,
+                props.mode.description,
+                "Error executing process",
+                it.message
+            )
         )
+    }
+
+    private fun onSuccess() = runBlocking {
+        notificationSender.send(
+            Report(
+                SYSTEM,
+                props.mode.description,
+                "Process was completed successfully"
+            )
+        )
+    }
+
+    private fun runProcess(mode: PmcMode): Unit {
+        when (mode) {
+            LOAD -> {
+                val folder = File(requireNotNull(props.loadFolder) { "load folder parameter is required" })
+                val file = props.loadFile?.let { folder.resolve(it) }
+                context.getBean<PmcFileLoader>().loadFile(folder, file)
+            }
+
+            PROCESS -> context.getBean<PmcProcessor>().processAll(props.sourceFile)
+            SUBMIT -> context.getBean<PmcSubmitter>().submitAll(props.sourceFile)
+            SUBMIT_SINGLE -> context.getBean<PmcSubmitter>().submitSingle(requireNotNull(props.submissionId))
+        }
     }
 }
