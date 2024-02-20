8e2f189c400723c6a0099357dc0cb7f2cca5032a
Jhoan Munoz <jhoanmanuelms@gmail.com>
2024-02-20 09:07:35 -0500

    Pivotal ID # 186710912: BCC-ing All E-mail Notifications
    - Fix broken test
    - Remove needless blank line

--- client/ftp-webclient/src/test/kotlin/ebi/ac/uk/ftp/FtpClientTest.kt
+++ client/ftp-webclient/src/test/kotlin/ebi/ac/uk/ftp/FtpClientTest.kt
@@ -1,5 +1,6 @@
 package ebi.ac.uk.ftp
 
+import ebi.ac.uk.test.createTempFile
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
