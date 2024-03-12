2c1f72de5f9bc8185c02935a41a0d265a93a40fd
Jhoan Munoz <jhoanmanuelms@gmail.com>
2024-03-12 10:29:29 -0500

    Pivotal ID # 186944806: Deprecate XML Pagetab
    - Remove serialization/deserialization related code
    - Remove XML iTests

--- commons/commons-serialization/src/main/kotlin/ac/uk/ebi/biostd/xml/deserializer/exception/InvalidXmlPageTabElementException.kt
+++
@@ -1,5 +0,0 @@
-package ac.uk.ebi.biostd.xml.deserializer.exception
-
-const val INVALID_XML_ELEMENT_ERROR = "The given element is not valid page tab"
-
-class InvalidXmlPageTabElementException : RuntimeException(INVALID_XML_ELEMENT_ERROR)
