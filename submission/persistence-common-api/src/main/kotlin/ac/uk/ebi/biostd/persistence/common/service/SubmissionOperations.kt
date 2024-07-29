6048ea38fdea93e1530cb49bbd18e1ae5c87d03b
Juan Camilo Rada Mesa <jcamilorada@ebi.ac.uk>
2024-07-29 08:22:09 -0500

    code review changes

--- submission/persistence-common-api/src/main/kotlin/ac/uk/ebi/biostd/persistence/common/service/SubmissionOperations.kt
+++ submission/persistence-common-api/src/main/kotlin/ac/uk/ebi/biostd/persistence/common/service/SubmissionOperations.kt
@@ -29,6 +29,7 @@ interface SubmissionPersistenceService {
     suspend fun getNextVersion(accNo: String): Int
 }
 
+@Suppress("TooManyFunctions")
 interface SubmissionPersistenceQueryService {
     suspend fun existByAccNo(accNo: String): Boolean
 
@@ -137,7 +138,10 @@ sealed interface OptResponse<T> {
     operator fun component2(): T = value
 }
 
-class RqtUpdate(override val rqt: SubmissionRequest, override val value: Unit = Unit) : OptResponse<Unit>
+class RqtUpdate(
+    override val rqt: SubmissionRequest,
+    override val value: Unit = Unit,
+) : OptResponse<Unit>
 
 class RqtResponse(
     override val rqt: SubmissionRequest,
