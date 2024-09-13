f1bb7dc9ae93017b1e8762313e450ccf15f99c55
Jhoan Munoz <jhoanmanuelms@gmail.com>
2024-09-13 17:08:16 +0100

    Pivotal ID # 187229710: DEV/BETA Backend To FTP/Public
    Adjust permissions for FTP out

--- submission/persistence-filesystem/src/main/kotlin/ac/uk/ebi/biostd/persistence/filesystem/extensions/FilePermissionsExtensions.kt
+++ submission/persistence-filesystem/src/main/kotlin/ac/uk/ebi/biostd/persistence/filesystem/extensions/FilePermissionsExtensions.kt
@@ -3,10 +3,9 @@ package ac.uk.ebi.biostd.persistence.filesystem.extensions
 import ebi.ac.uk.extended.model.ExtSubmissionInfo
 import ebi.ac.uk.io.Permissions
 import ebi.ac.uk.io.RWXR_XR_X
-import ebi.ac.uk.io.RWXR_X___
+import ebi.ac.uk.io.RWXR_X__X
 import ebi.ac.uk.io.RW_R__R__
-import ebi.ac.uk.io.RW_R_____
 
 fun ExtSubmissionInfo.permissions(): Permissions {
-    return if (released) Permissions(RW_R__R__, RWXR_XR_X) else Permissions(RW_R_____, RWXR_X___)
+    return if (released) Permissions(RW_R__R__, RWXR_XR_X) else Permissions(RW_R__R__, RWXR_X__X)
 }
