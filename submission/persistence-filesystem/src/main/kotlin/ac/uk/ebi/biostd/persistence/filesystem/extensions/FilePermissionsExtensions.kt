e32be147bce4fcfc0fcb4d6a1477cc7eadc1cb79
Jhoan Munoz <jhoanmanuelms@gmail.com>
2024-09-17 16:24:49 +0100

    Pivotal ID # 187229710: DEV/BETA Backend To FTP/Public
    Adjust permissions

--- submission/persistence-filesystem/src/main/kotlin/ac/uk/ebi/biostd/persistence/filesystem/extensions/FilePermissionsExtensions.kt
+++ submission/persistence-filesystem/src/main/kotlin/ac/uk/ebi/biostd/persistence/filesystem/extensions/FilePermissionsExtensions.kt
@@ -6,6 +6,12 @@ import ebi.ac.uk.io.RWXR_XR_X
 import ebi.ac.uk.io.RWXR_X__X
 import ebi.ac.uk.io.RW_R__R__
 
+/**
+ * Provide the expected permissions for the submission files based on its release status.
+ *
+ * - Public Submissions: All files should be readable. Directories should be readable and listable
+ * - Private Submissions: All files should be readable. Directories should be readable but not listable
+ */
 fun ExtSubmissionInfo.permissions(): Permissions {
     return if (released) Permissions(RW_R__R__, RWXR_XR_X) else Permissions(RW_R__R__, RWXR_X__X)
 }
