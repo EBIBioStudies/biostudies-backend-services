d95f3928082ea85889c6e00716d6a0d62320c2b5
Juan Camilo Rada Mesa <jcamilorada@ebi.ac.uk>
2024-09-27 15:23:34 -0500

    code review changes

--- client/cluster-client/src/main/kotlin/uk/ac/ebi/biostd/client/cluster/common/JobResponseParser.kt
+++ client/cluster-client/src/main/kotlin/uk/ac/ebi/biostd/client/cluster/common/JobResponseParser.kt
@@ -7,12 +7,18 @@ private val slurmResponseRegex = "Submitted batch job (\\d+)\\s*\$".toRegex()
 
 fun toLsfJob(response: String): Job {
     val matches = lsfResponseRegex.findAll(response).toList()
-    if (matches.size == 2) return Job(matches[0].groupValues[1], matches[1].groupValues[1])
+    if (matches.size == 2) return Job(matches[0].groupValues[1], matches[1].groupValues[1], "not-specified")
     error("could not parse response, '$response'")
 }
 
-fun toSlurmJob(response: String): Job {
+fun toSlurmJob(
+    response: String,
+    logsPath: String,
+): Job {
     val match = slurmResponseRegex.matchEntire(response)
-    val job = match?.destructured?.let { (jobId) -> Job(jobId, "not-specified") }
+    val job =
+        match?.destructured?.let { (jobId) ->
+            Job(id = jobId, queue = "not-specified", logsPath = "$logsPath/${jobId.takeLast(3)}/${jobId}_OUT")
+        }
     return job ?: error("could not parse response, '$response'")
 }
--- client/cluster-client/src/main/kotlin/uk/ac/ebi/biostd/client/cluster/model/Job.kt
+++ client/cluster-client/src/main/kotlin/uk/ac/ebi/biostd/client/cluster/model/Job.kt
@@ -3,4 +3,5 @@ package uk.ac.ebi.biostd.client.cluster.model
 data class Job(
     val id: String,
     val queue: String,
+    val logsPath: String,
 )
