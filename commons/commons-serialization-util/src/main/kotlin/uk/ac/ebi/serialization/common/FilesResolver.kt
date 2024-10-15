a507f4bfa339cb577d148e1d85925aff1479fd4e
Jhoan Munoz <jhoanmanuelms@gmail.com>
2024-10-15 15:03:18 +0100

    Pivotal ID # 181219386: Clean Submission Temp Files
    - Delete files in the request-files folder
    - Create temp files for simple submission read in the system temp folder rather than the request folder

--- commons/commons-serialization-util/src/main/kotlin/uk/ac/ebi/serialization/common/FilesResolver.kt
+++ commons/commons-serialization-util/src/main/kotlin/uk/ac/ebi/serialization/common/FilesResolver.kt
@@ -1,12 +1,8 @@
 package uk.ac.ebi.serialization.common
 
 import ebi.ac.uk.io.ext.createTempFile
-import ebi.ac.uk.io.ext.resolveMany
 import java.io.File
-import java.time.LocalDate
-import java.time.temporal.ChronoField.DAY_OF_MONTH
-import java.time.temporal.ChronoField.MONTH_OF_YEAR
-import java.time.temporal.ChronoField.YEAR
+import java.nio.file.Files
 
 class FilesResolver(private val basePath: File) {
     fun createExtEmptyFile(
@@ -15,23 +11,14 @@ class FilesResolver(private val basePath: File) {
         fileName: String,
     ): File {
         val name = fileName.replace("/", "-")
-        return baseFolder(subAccNo, version.toString()).createTempFile(name, ".json")
+        val baseFolder = basePath.resolve("$subAccNo/$version")
+        baseFolder.mkdirs()
+
+        return baseFolder.createTempFile(name, ".json")
     }
 
     fun createEmptyFile(fileName: String): File {
         val name = fileName.replace("/", "-")
-        return baseFolder("submissions").createTempFile(name, ".json")
-    }
-
-    private fun baseFolder(vararg keys: String): File {
-        val now = LocalDate.now()
-        val path =
-            basePath
-                .resolve(now.get(YEAR).toString())
-                .resolve(now.get(MONTH_OF_YEAR).toString())
-                .resolve(now.get(DAY_OF_MONTH).toString())
-                .resolveMany(*keys)
-        path.mkdirs()
-        return path
+        return Files.createTempFile(name, ".json").toFile()
     }
 }
