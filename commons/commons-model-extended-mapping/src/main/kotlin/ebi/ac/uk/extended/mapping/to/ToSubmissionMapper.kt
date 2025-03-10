d330046bda5735760d041c4033cbafddcda57f42
Jhoan Munoz <jhoanmanuelms@gmail.com>
2025-03-10 11:47:18 +0000

    Pivotal ID # 187898907: Double Blind Review PageTab Generation
    Refactor filtering logic

--- commons/commons-model-extended-mapping/src/main/kotlin/ebi/ac/uk/extended/mapping/to/ToSubmissionMapper.kt
+++ commons/commons-model-extended-mapping/src/main/kotlin/ebi/ac/uk/extended/mapping/to/ToSubmissionMapper.kt
@@ -1,7 +1,10 @@
 package ebi.ac.uk.extended.mapping.to
 
+import ebi.ac.uk.base.Either
 import ebi.ac.uk.extended.model.ExtSubmission
 import ebi.ac.uk.model.Attribute
+import ebi.ac.uk.model.Section
+import ebi.ac.uk.model.SectionsTable
 import ebi.ac.uk.model.Submission
 import ebi.ac.uk.model.constants.SubFields.ATTACH_TO
 import ebi.ac.uk.model.constants.SubFields.COLLECTION_VALIDATOR
@@ -10,19 +13,50 @@ import ebi.ac.uk.model.constants.SubFields.PUBLIC_ACCESS_TAG
 import ebi.ac.uk.model.constants.SubFields.RELEASE_DATE
 import ebi.ac.uk.model.constants.SubFields.ROOT_PATH
 import ebi.ac.uk.model.constants.SubFields.TITLE
+import ebi.ac.uk.model.extensions.isAuthor
+import ebi.ac.uk.model.extensions.isOrganization
+import ebi.ac.uk.model.extensions.reviewType
 
-class ToSubmissionMapper(private val toSectionMapper: ToSectionMapper) {
+class ToSubmissionMapper(
+    private val toSectionMapper: ToSectionMapper,
+) {
     /**
-     * Return a simple submission which does not contain submission secret information. Used to generate public
-     * submission tab files.
+     * Return a simple submission which does not contain submission extended information.
      */
-    suspend fun toSimpleSubmission(sub: ExtSubmission): Submission =
-        Submission(
-            accNo = sub.accNo,
-            section = toSectionMapper.convert(sub.section),
-            attributes = sub.simpleAttributes(),
-            tags = sub.tags.mapTo(mutableListOf()) { Pair(it.name, it.value) },
-        )
+    suspend fun toSimpleSubmission(
+        sub: ExtSubmission,
+        filterBlindReview: Boolean = false,
+    ): Submission {
+        val simpleSubmission =
+            Submission(
+                accNo = sub.accNo,
+                section = toSectionMapper.convert(sub.section),
+                attributes = sub.simpleAttributes(),
+                tags = sub.tags.mapTo(mutableListOf()) { Pair(it.name, it.value) },
+            )
+
+        if (filterBlindReview && sub.released.not() && simpleSubmission.reviewType == DOUBLE_BLIND) {
+            simpleSubmission.section = filterBlindReview(simpleSubmission.section) ?: Section()
+        }
+
+        return simpleSubmission
+    }
+
+    private fun filterBlindReview(section: Section): Section? {
+        if (section.isAuthor() || section.isOrganization()) return null
+
+        val sections =
+            section.sections.mapNotNull { either ->
+                either.fold(
+                    { section -> filterBlindReview(section)?.let { Either.Left(it) } },
+                    { table -> Either.Right(SectionsTable(table.elements.mapNotNull { filterBlindReview(it) })) },
+                )
+            }
+
+        section.sections = sections.toMutableList()
+
+        return section
+    }
 
     private fun ExtSubmission.simpleAttributes(): List<Attribute> =
         buildSet {
@@ -33,4 +67,8 @@ class ToSubmissionMapper(private val toSectionMapper: ToSectionMapper) {
             rootPath?.let { add(Attribute(ROOT_PATH.value, it)) }
             addAll(collections.filter { it.accNo != PUBLIC_ACCESS_TAG.value }.map { Attribute(ATTACH_TO.value, it.accNo) })
         }.toList()
+
+    companion object {
+        const val DOUBLE_BLIND = "DoubleBlind"
+    }
 }
--- submission/persistence-filesystem/src/main/kotlin/ac/uk/ebi/biostd/persistence/filesystem/pagetab/PageTabUtil.kt
+++ submission/persistence-filesystem/src/main/kotlin/ac/uk/ebi/biostd/persistence/filesystem/pagetab/PageTabUtil.kt
@@ -4,7 +4,6 @@ import ac.uk.ebi.biostd.integration.SerializationService
 import ac.uk.ebi.biostd.integration.SubFormat.Companion.JSON_PRETTY
 import ac.uk.ebi.biostd.integration.SubFormat.Companion.TSV
 import ac.uk.ebi.biostd.persistence.filesystem.extensions.permissions
-import ebi.ac.uk.base.Either
 import ebi.ac.uk.extended.mapping.to.ToFileListMapper
 import ebi.ac.uk.extended.mapping.to.ToSubmissionMapper
 import ebi.ac.uk.extended.model.ExtFileList
@@ -13,11 +12,6 @@ import ebi.ac.uk.extended.model.allFileList
 import ebi.ac.uk.io.FileUtils
 import ebi.ac.uk.io.Permissions
 import ebi.ac.uk.io.ext.newFile
-import ebi.ac.uk.model.Section
-import ebi.ac.uk.model.SectionsTable
-import ebi.ac.uk.model.extensions.isAuthor
-import ebi.ac.uk.model.extensions.isOrganization
-import ebi.ac.uk.model.extensions.reviewType
 import java.io.File
 
 class PageTabUtil(
@@ -29,12 +23,7 @@ class PageTabUtil(
         extSub: ExtSubmission,
         target: File,
     ): PageTabFiles {
-        val sub = toSubmissionMapper.toSimpleSubmission(extSub)
-
-        if (extSub.released.not() && sub.reviewType == DOUBLE_BLIND) {
-            sub.section = filterBlindReview(sub.section) ?: Section()
-        }
-
+        val sub = toSubmissionMapper.toSimpleSubmission(extSub, filterBlindReview = true)
         val permissions = extSub.permissions()
 
         return PageTabFiles(
@@ -51,22 +40,6 @@ class PageTabUtil(
                     permissions,
                 ),
         )
-    }
-
-    private fun filterBlindReview(section: Section): Section? {
-        if (section.isAuthor() || section.isOrganization()) return null
-
-        val sections =
-            section.sections.mapNotNull { either ->
-                either.fold(
-                    { section -> filterBlindReview(section)?.let { Either.Left(it) } },
-                    { table -> Either.Right(SectionsTable(table.elements.mapNotNull { filterBlindReview(it) })) },
-                )
-            }
-
-        section.sections = sections.toMutableList()
-
-        return section
     }
 
     suspend fun generateFileListPageTab(
@@ -101,10 +74,6 @@ class PageTabUtil(
     ): File {
         FileUtils.writeContent(file, content, permissions)
         return file
-    }
-
-    companion object {
-        const val DOUBLE_BLIND = "DoubleBlind"
     }
 }
 
