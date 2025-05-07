d4dce788084de2e2bb1bb35b7822bb5273cb08bf
Jhoan Munoz <jhoanmanuelms@gmail.com>
2025-05-07 14:52:12 +0100

    Pivotal # 188837373: Move Applications To Production NFS
    Set Java home and temporary files directory as configurable properties

--- ci/update.sh
+++ ci/update.sh
@@ -15,7 +15,7 @@ cd $(dirname $0)
 
 # Obtain the process id by checking any process running with the application name
 PID=$(pgrep -f APP_PATH/APP_NAME)
-JAVA_HOME=/nfs/biostudies/.adm/java/zulu17.50.19-ca-jdk17.0.11-linux_x64
+JAVA_HOME=JAVA_DIR_PATH
 
 # Kill and wait for process to be finished
 while $(kill -9 ${PID} 2>/dev/null); do sleep 1;done;
@@ -24,6 +24,7 @@ mv APP_PATH/start_logs.txt APP_PATH/logs/start_logs_$(date +%s).txt
 ## deploy new version of application
 nohup ${JAVA_HOME}/bin/java JVM_PARAMS \
     -Dsun.jnu.encoding=UTF-8 \
+    -Djava.io.tmpdir=TEMP_FILES_DIR_PATH \
     -Xdebug -Xrunjdwp:transport=dt_socket,server=y,address=0.0.0.0:DEBUG_PORT,suspend=n \
     -jar APP_PATH/APP_NAME \
     --spring.config.location=APP_PATH/application.yml \
