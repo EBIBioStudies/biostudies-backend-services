198d3d88009ab5eaeaaad5a98697ef525acd394e
Jhoan Manuel Muñoz Serrano <jhoanmanuelms@gmail.com>
2025-07-11 11:18:31 +0100

    BIOSTD-264: Discontinue Sandbox Environment (#963)
    https://embl.atlassian.net/browse/BIOSTD-264
    
    - Remove Sandbox environment from the deployment process
    - Add logging for the tmp folder cleaning process
    - Fix typos

--- commons/commons-util/src/main/kotlin/ebi/ac/uk/system/SystemExt.kt
+++ commons/commons-util/src/main/kotlin/ebi/ac/uk/system/SystemExt.kt
@@ -4,8 +4,6 @@ import java.nio.file.Path
 import java.nio.file.Paths
 
 /**
- * Obtain the current user configured temp folder.
+ * Get the current user configured temp folder.
  */
-fun tempFolder(): Path {
-    return Paths.get(System.getProperty("java.io.tmpdir"))
-}
+fun tempFolder(): Path = Paths.get(System.getProperty("java.io.tmpdir"))
