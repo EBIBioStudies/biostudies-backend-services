da3c7aa7f21d0d5961440f3411f17123f32e0484
Juan <jcamilorada@ebi.ac.uk>
2026-04-20 16:21:51 +0100

    BIOSTD-387: Mantainance operations in migrator (#1037)
    https://embl.atlassian.net/browse/BIOSTD-387
    - moved cleaning to migrator

--- submission/submission-webapp/src/main/kotlin/ac/uk/ebi/biostd/admin/operations/OperationsScheduler.kt
+++ submission/submission-webapp/src/main/kotlin/ac/uk/ebi/biostd/admin/operations/OperationsScheduler.kt
@@ -1,10 +1,12 @@
 package ac.uk.ebi.biostd.admin.operations
 
+import ac.uk.ebi.biostd.common.properties.ApplicationProperties
 import ac.uk.ebi.biostd.submission.stats.service.StatsReporterService
 import kotlinx.coroutines.runBlocking
 import org.springframework.scheduling.annotation.Scheduled
 
 class OperationsScheduler(
+    private val applicationProperties: ApplicationProperties,
     private val operationsService: OperationsService,
     private val statsReporterService: StatsReporterService,
 ) {
@@ -12,19 +14,25 @@ class OperationsScheduler(
      * Delete request files every day at 1 am.
      */
     @Scheduled(cron = "0 0 1 * * *")
-    fun deleteRequestFiles() = runBlocking { operationsService.deleteRequestFiles() }
+    fun deleteRequestFiles() {
+        runBlocking { if (applicationProperties.enableTmpCleaning) operationsService.deleteRequestFiles() }
+    }
 
     /**
      * Archive request every day at 4 am.
      */
     @Scheduled(cron = "0 0 4 * * *")
-    fun archiveRequests() = runBlocking { operationsService.archiveRequests() }
+    fun archiveRequests() {
+        runBlocking { if (applicationProperties.enableTmpCleaning) operationsService.archiveRequests() }
+    }
 
     /**
      * Clean files in temp folders every week, on Sundays, at 5 am
      */
     @Scheduled(cron = "0 0 5 * * 0")
-    fun cleanTempFolders() = runBlocking { operationsService.cleanTempFolders() }
+    fun cleanTempFolders() {
+        runBlocking { if (applicationProperties.enableTmpCleaning) operationsService.cleanTempFolders() }
+    }
 
     /**
      * Generate stats report on the 4th day of every month at 3:00 AM
