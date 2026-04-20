8b4274557f2b002d2ececaf092bf747cd1d547da
Jhoan Munoz <jhoanmanuelms@gmail.com>
2026-04-20 17:02:47 +0100

    BIOSTD-312: Implement Statistics Report Generation and API
    - Remove stats reporter from the scheduler module
    - Fix typos

--- scheduler/scheduler/src/main/kotlin/uk/ac/ebi/scheduler/scheduling/DailyScheduler.kt
+++ scheduler/scheduler/src/main/kotlin/uk/ac/ebi/scheduler/scheduling/DailyScheduler.kt
@@ -6,7 +6,6 @@ import uk.ac.ebi.scheduler.common.properties.DailyScheduling
 import uk.ac.ebi.scheduler.pmc.exporter.domain.ExporterTrigger
 import uk.ac.ebi.scheduler.pmc.importer.domain.PmcLoaderService
 import uk.ac.ebi.scheduler.releaser.domain.SubmissionReleaserTrigger
-import uk.ac.ebi.scheduler.stats.domain.StatsReporterTrigger
 
 const val DAILY_PMC_SUB_LIMIT = 5_000
 
@@ -14,7 +13,6 @@ internal class DailyScheduler(
     private val dailyScheduling: DailyScheduling,
     private val exporterTrigger: ExporterTrigger,
     private val pmcLoaderService: PmcLoaderService,
-    private val statsReporterTrigger: StatsReporterTrigger,
     private val submissionReleaserTrigger: SubmissionReleaserTrigger,
 ) {
     // Execute at 02:00 am
@@ -71,12 +69,5 @@ internal class DailyScheduler(
     fun exportPublicSubmissions() =
         runBlocking {
             if (dailyScheduling.exporter) exporterTrigger.triggerPublicExport()
-        }
-
-    // 3:00 AM on the 4th day of every month
-    @Scheduled(cron = "0 0 3 4 * *")
-    fun publishSubmissionStatsReport() =
-        runBlocking {
-            if (dailyScheduling.statsReporter) statsReporterTrigger.triggerStatsReporter()
         }
 }
