23506206b861b09aa84849829eaf34abe3cd62d0
Juan Camilo Rada Mesa <jcamilorada@ebi.ac.uk>
2026-04-21 16:38:46 +0100

    changes

--- commons/commons-util/src/main/kotlin/ebi/ac/uk/io/Streams.kt
+++ commons/commons-util/src/main/kotlin/ebi/ac/uk/io/Streams.kt
@@ -4,10 +4,8 @@ package ebi.ac.uk.io
  * Allow to execute the given instances of autocloseable and guarantee their disposal after function execution.
  * Equivalent to kotlin @see [AutoCloseable.use].
  */
-inline fun <A : AutoCloseable, B : AutoCloseable> use(
+inline fun <A : AutoCloseable, B : AutoCloseable, T> use(
     a: A,
     b: B,
-    func: (A, B) -> Unit,
-) {
-    a.use { b.use { func(a, b) } }
-}
+    func: (A, B) -> T,
+): T = a.use { b.use { func(a, b) } }
